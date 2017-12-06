e6c732d2c4a7735689e19266abc2d18a762954ea
Velislav Slavov <slavov.vili@gmail.com>
2017-12-06 14:23:14 +0100

    Finished day 1 - part 2

--- day_1/captcha_sum.go
+++ day_1/captcha_sum.go
@@ -9,22 +9,55 @@ import (
 
 
 func main() {
+    // stores which part of the assignment is being run
+    var part int
     // create a scanner to read the input
     var scanner = bufio.NewScanner(os.Stdin);
+    // convert the very first input to an int and store it
+    if (scanner.Scan()) {
+        part,_ = strconv.Atoi(scanner.Text())
+    }   //end if
 
     // while input is being received
     for (scanner.Scan()) {
-        var sum = 0;
         // store the input
         var input = scanner.Text();
+        run_captcha_calc(input, part)
+    }   //end for
+}   //end main
+
+
+
+// Run the captcha calculator, taking into account which part of the assignment is being tested
+//
+// Arguments:
+// input - the input to the calculator
+// part  - which part of the assignment is being tested
+//
+// Returns:
+// the sum of equal numbers based on the criterion
+func run_captcha_calc(input string, part int) {
+        var sum = 0;
         // split the input into characters, because strings are weird in Go
         var input_chars = strings.Split(input, "");
         var input_len   = len(input_chars);
 
+        // store the function to calculate the next relevant index
+        // based on which part of the assignment is being run
+        var calc_idx func(int, int) int
+        // if an invalid part is given
+        if ((part < 1) || (part > 2)) {
+            println("Invalid part of the assignment !")
+            os.Exit(0);
+        }   //end if
+        // store the correct function for the particular part of the assignment
+        if (part == 1) { calc_idx = calc_next_idx }
+        if (part == 2) { calc_idx = calc_mid_idx  }
+
         // for each character in the input
         for i:=0; i<input_len; i++ {
             // calculate the index of the next number to be considered
-            var y = calc_next_idx(i, input_len);
+            var y = calc_idx(i, input_len);
             // if this number and the next one are equal, add it to the sum
 
             if (strings.Compare(input_chars[i], input_chars[y]) == 0) {
@@ -38,13 +71,12 @@ func main() {
 
         print("Sum: ");
         print(sum);
-        printl()
-    }   //end for
-}   //end main
+        println()
+}   //end func
+
 
 
 // Calculates the index of the next number in the captcha.
-// The captcha is just a sequence of integers.
 //
 // Arguments:
 // cur_idx - the index of the current number in the captcha
@@ -53,6 +85,18 @@ func main() {
 // Returns:
 // the index of the next number to be considered in the captcha
 func calc_next_idx(cur_idx, length int) int {
-    return (cur_idx + 1) % length;
+    return ((cur_idx + 1) % length);
 }   //end func
 
+
+// Calculates the index of the number n/2 steps away from the current one.
+//
+// Arguments:
+// cur_idx - the index of the current number in the captcha
+// length  - the length of the captcha
+//
+// Returns:
+// the index of the number n/2 steps further in the captcha
+func calc_mid_idx(cur_idx, length int) int {
+    return ((cur_idx + (length/2)) % length)
+}   //end func
