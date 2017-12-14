d4028addf791cbbd809b385c74da909ade955c61
Velislav Slavov <slavov.vili@gmail.com>
2017-12-14 15:11:47 +0100

    Fixed day 9 - part 1

--- day_9/group_counter.go
+++ day_9/group_counter.go
@@ -1,61 +1,63 @@
 package main
 
 import (
-    "bufio"
-    //"fmt"
+    "io/ioutil"
     "os"
-    "strings"
 )   //end imports
 
 
 func main() {
-    // create a scanner to read the input
-    var scanner = bufio.NewScanner(os.Stdin);
+    var args = os.Args;
 
-    // while input is being received
-    println("Input:")
-    for scanner.Scan() {
-        // store the input
-        var input_chars = strings.Split(scanner.Text(), "");
+    // store the input
+    var input,read_err = ioutil.ReadFile(args[1]);
+    if read_err != nil {
+        println("ERROR: ", read_err.Error())
+    }   //end if
 
-        // run the group counter and get the total score
-        var score = run_group_counter(input_chars);
-        print("Total score: ")
-        print(score)
-        println()
-    }   //end for
+    // run the group counter and get the total score
+    var score = run_group_counter(string(input));
+    print("Total score: ")
+    print(score)
+    println()
 }   //end main
 
 
 
-func run_group_counter(input []string) (score int) {
+func run_group_counter(input string) (score int) {
         // counts how many groups have been opened
-        var groups_started = 0;
-        var trash_has_started  = false;
+        var total_groups_started, total_groups_closed int;
+        var total_trashes_started, total_trashes_closed int;
+        var groups_started int;
+        var trash_has_started bool;
 
         // for each character in the input
         for i, char := range input {
             switch {
             // if the character is the beginning of trash
             // and it is outside of trash
-            case (char == "<") && (trash_has_started == false): {
+            case (char == '<') && (trash_has_started == false): {
                 // tell the program, that trash content has started
                 trash_has_started = true;
+                total_trashes_started++;
             }   //end case
             // if the character is the un-escaped end of trash
-            case (char == ">") && (is_escaped(input, i) == false): {
+            case (char == '>') && (is_escaped(input, i) == false): {
                 // tell the program, that trash content has ended
                 trash_has_started = false;
+                total_trashes_closed++;
             }   //end case
             // if the character is the beginning of a group
             // and it is outside trash
-            case (char == "{") && (trash_has_started == false): {
+            case (char == '{') && (trash_has_started == false): {
+                total_groups_started++;
                 // save the starting index of the group
                 groups_started++;
             }   //end case
             // if the character is the end of a group
             // and it is outside trash
-            case (char == "}") && (trash_has_started == false): {
+            case (char == '}') && (trash_has_started == false): {
+                total_groups_closed++;
                 score += groups_started;
                 // close the last opened group
                 groups_started--;
@@ -69,10 +71,10 @@ func run_group_counter(input []string) (score int) {
 
 
 // checks whether the character with index 'idx' in the contents is escaped
-func is_escaped(input []string, idx int) (result bool) {
-    var prev_idx = idx -1;
+func is_escaped(input string, idx int) (result bool) {
+    var prev_idx = idx - 1;
     // while the previous character is the escape character
-    for input[prev_idx] == "!" {
+    for input[prev_idx] == '!' {
         result = !result;
         prev_idx -= 1;
     }   //end for
