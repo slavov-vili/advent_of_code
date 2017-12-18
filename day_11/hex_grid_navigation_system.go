f5f4c613b69462e184605f5120edce71908bbf95
Velislav Slavov <slavov.vili@gmail.com>
2017-12-18 16:22:03 +0100

    Finished day 11 - part 1

--- day_11/hex_grid_navigation_system.go
+++ day_11/hex_grid_navigation_system.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-    "fmt"
+    //"fmt"
     "io/ioutil"
     "log"
     "os"
@@ -18,15 +18,23 @@ func main() {
         log.Fatal(read_err);
     }   //end if
 
-    // store the initial path through the grid
-    var path    = strings.Split(string(input), ",");
-    //var path    = strings.Split("ne,ne,ne", ",");
-    var changes = -1;
+    // store the initial path through the grid (remove the 'newline' symbol)
+    var path    = strings.Split(string(input[:(len(input) - 1)]), ",");
+    //var path    = strings.Split("n,ne,s", ",");
+    var prev_path_len = 0;
+    var cur_path_len  = len(path);
 
-    // while changes were made during path optimization
-    for changes != 0 {
-        path, changes = optimize_path(path);
+    // while differences are being made to the paths
+    for prev_path_len != cur_path_len {
+        // optimize the path
+        path = optimize_path(path);
+        //fmt.Println("Path: ", path)
+        // reset the lengths of the path
+        prev_path_len = cur_path_len;
+        cur_path_len = len(path);
     }   //end for
+    //fmt.Println("Final Path: ", path)
+
     print("Total moves needed: ")
     print(len(path))
     println()
@@ -35,8 +43,8 @@ func main() {
 
 
 // tries to optimize the given path
-// returns the optimized path AND the number of changes made
-func optimize_path(path []string) (path_optimized []string, changes int) {
+// returns the optimized path
+func optimize_path(path []string) (path_optimized []string) {
     // iterate over the path
     for i := 0; i < len(path); i++ {
         var cur_step = path[i]
@@ -45,60 +53,60 @@ func optimize_path(path []string) (path_optimized []string, changes int) {
         if cur_step != "_" {
 
             // if we're at the end of the path
-            if (i == (len(path) - 1)) {
+            if i == (len(path) - 1) {
                 path_optimized = append(path_optimized, cur_step);
             } else {
+                // get all steps after this one
                 var next_steps = path[(i + 1):];
+                // if there are NO more unreduced steps after this one
+                if get_real_length(next_steps) == 0 {
+                    // add the current step the the optimized path (can't reduce when the rest is empty)
+                    path_optimized = append(path_optimized, cur_step);
+                } else {
+                    // for each step after the current one
+                    for j := 0; j < len(next_steps); j++ {
+                        var next_step = next_steps[j];
 
-                // for each step after the current one
-                for y := 0; y < len(next_steps); y++ {
-                    var next_step = next_steps[y];
+                        if next_step != "_" {
+                            // attempt to reduce the two steps
+                            var steps_reduced = reduce_steps(cur_step, next_step);
 
-                    if next_step != "_" {
-                        // attempt to reduce the two steps
-                        var steps_reduced = reduce_steps(cur_step, next_step);
-
-                        // if the steps were reduced to nothing (moving in opposite directions)
-                        if len(steps_reduced) == 0 {
-                            // note that a change has been made
-                            changes++;
-                            // mark that the two steps were used for a reduction
-                            path[i]       = "_";
-                            next_steps[y] = "_";
-                            //fmt.Println("Reduced '", cur_step, "' and '", next_step, "' to: ", steps_reduced)
-                            break;
-                        // if the two steps were successfully reduced to 1
-                        } else if len(steps_reduced) == 1 {
-                            // note that a change has been made
-                            changes++;
-                            // add the result of the step reduction to the optimized path
-                            path_optimized = append(path_optimized, steps_reduced[0]);
-                            // mark that the two steps were used for a reduction
-                            path[i]       = "_";
-                            next_steps[y] = "_";
-                            //fmt.Println("Reduced '", cur_step, "' and '", next_step, "' to: ", steps_reduced)
-                            break;
-                        // if the reduction was impossible
-                        } else if len(steps_reduced) == 2 {
-                            // if there are no more non-reduced steps in the list of next steps
-                            if get_real_length(next_steps[y:]) == 1 {
-                                // add the first step to the optimized path list
-                                // (next step gets added automatically later)
-                                path_optimized = append(path_optimized, steps_reduced[0]);
+                            // if the steps were reduced to nothing (moving in opposite directions)
+                            if len(steps_reduced) == 0 {
+                                // mark that the two steps were used for a reduction
+                                path[i]       = "_";
+                                next_steps[j] = "_";
                                 break;
+                            // if the two steps were successfully reduced to 1
+                            } else if len(steps_reduced) == 1 {
+                                // add the result of the step reduction to the optimized path
+                                path_optimized = append(path_optimized, steps_reduced[0]);
+                                // mark that the two steps were used for a reduction
+                                path[i]       = "_";
+                                next_steps[j] = "_";
+                                break;
+                            // if the reduction was impossible
+                            } else if len(steps_reduced) == 2 {
+                                // if this is the last non-reduced step in the list of next steps
+                                if get_real_length(next_steps[j:]) == 1 {
+                                    // add the first step to the optimized path list
+                                    // (next step gets added automatically later)
+                                    path_optimized = append(path_optimized, steps_reduced[0]);
+                                    break;
+                                } else {
+                                    continue;
+                                }   //end else
                             } else {
-                                continue;
+                                log.Fatal("Error in step reduction");
                             }   //end else
-                        } else {
-                            log.Fatal("Error in step reduction");
-                        }   //end else
-                    }   //end if
-                }   //end for
+                        }   //end if
+                    }   //end for
+                }   //end else
             }   //end else
         }   // end if
     }   //end for
 
-    fmt.Println("Optimized path: ", path_optimized)
+    //fmt.Println("Optimized path: ", path_optimized)
     return;
 }   //end func
 
@@ -134,6 +142,7 @@ func reduce_steps(step1, step2 string) (new_steps []string) {
         // if its none of the above (the steps cannot be reduced, return the input steps)
         default: new_steps = append(new_steps, step1, step2);
     }   //end switch
+    //fmt.Println("Reduced '", step1, "' and '", step2, "' to: ", new_steps)
     return;
 }   //end func
 
