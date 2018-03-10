55b98c6d9212136167b0613a8c1afaec03e073dd
vilskin <slavov.vili@gmail.com>
2018-03-10 16:31:15 +0100

    Finished day 18 - part 2

--- day_18/assembly_interpreter.go
+++ day_18/assembly_interpreter.go
@@ -46,6 +46,10 @@ func main() {
 
 
         // PART II
+        // find the number of sends that program '1' made
+        var prog_1_send_count = find_prog_1_send_count(instructions);
+        // print out the count
+        fmt.Println("Program '1' sent", prog_1_send_count, "values !");
 
 
         // clear the instruction list
@@ -113,29 +117,87 @@ func find_prog_1_send_count(instructions []*Instruction) (prog_1_send_count int)
     // stores the values to be received by program '1'
     var prog_1_inbox = make([]int, 0)
 
+
     OUTER:
     for {
         // whether either program is terminated or receiving a value
-        var prog_0_term_or_rec = prog_0.IsTerminated(len(instructions)) || prog_0.is_receiving;
-        var prog_1_term_or_rec = prog_1.IsTerminated(len(instructions)) || prog_1.is_receiving;
+        var prog_0_term_or_rec = prog_0.IsTerminated(len(instructions)) || (prog_0.is_receiving && len(prog_0_inbox) == 0);
+        var prog_1_term_or_rec = prog_1.IsTerminated(len(instructions)) || (prog_1.is_receiving && len(prog_1_inbox) == 0);
 
-        // if neither of the programs are capable of executing instructions
+        // if NEITHER of the programs is capable of executing instructions
         if prog_0_term_or_rec && prog_1_term_or_rec {
             break OUTER;
         }   //end if
 
-        // if program '0' hasn't terminated yet
-        if !prog_0.IsTerminated(len(instructions)) {
-            // if the program is waiting to receive a value
-            if prog_0.is_receiving {
-                if len(prog_1_inbox) != 0 {
-                    // TODO: fix this
-                }   //end if
-            } else {
-            
-            }   //end if
+        // handle instructions with program '0' until a "receive" instruction is reached and there is nothing to receive
+        prog_0_inbox, prog_1_inbox = handle_instructions_until_empty_inbox(prog_0, instructions, prog_0_inbox, prog_1_inbox);
+
+        // store the size of program '0''s inbox
+        var prog_0_old_inbox_size = len(prog_0_inbox);
+        // handle instructions with program '1' until a "receive" instruction is reached and there is nothing to receive
+        prog_1_inbox, prog_0_inbox = handle_instructions_until_empty_inbox(prog_1, instructions, prog_1_inbox, prog_0_inbox);
+        // if after handling the instructions, the inbox of program '0' is bigger
+        if prog_0_old_inbox_size < len(prog_0_inbox) {
+            // increase the count of sent values
+            prog_1_send_count += (len(prog_0_inbox) - prog_0_old_inbox_size);
         }   //end if
     }   //end for
+
+    return;
+}   //end func
+
+
+// handles instructions for the given program until a "receive" instruction is reached and there is nothing to receive
+func handle_instructions_until_empty_inbox(prog_a *Program, instructions []*Instruction, prog_a_inbox []int, prog_b_inbox []int) (new_prog_a_inbox, new_prog_b_inbox []int) {
+        // clone the two inboxes so we can edit them in peace :)
+        new_prog_a_inbox = clone_int_array(prog_a_inbox);
+        new_prog_b_inbox = clone_int_array(prog_b_inbox);
+
+
+        OUTER:
+        // while program 'a' hasn't terminated yet
+        for !prog_a.IsTerminated(len(instructions)) {
+            // store the instruction which program 'a' is currently executing
+            var cur_instruction = instructions[prog_a.cur_instruction_idx];
+
+
+            // handle the instruction
+            was_jump, err := prog_a.HandleInstruction(cur_instruction);
+            if err != nil {
+                switch cur_instruction.name {
+
+                case "snd":
+                    // add the value to the OTHER program's inbox
+                    new_prog_b_inbox = append(new_prog_b_inbox, get_arg_value(cur_instruction.args[0], prog_a.registers));
+
+                case "rcv":
+                    // if there is NO value to be received
+                    if len(new_prog_a_inbox) == 0 {
+                        // set the flag to true
+                        prog_a.is_receiving = true;
+                        // stop iterating through instructions
+                        break OUTER;
+                        // if a value CAN be received
+                    } else {
+                        // set the flag to false
+                        prog_a.is_receiving = false;
+
+                        // store the name of the register, where the received value should be stored
+                        var reg_name = cur_instruction.args[0];
+                        // store the first value in the inbox to the register
+                        prog_a.registers[reg_name] = new_prog_a_inbox[0];
+
+                        // remove the value from THIS program's inbox
+                        new_prog_a_inbox = new_prog_a_inbox[1:];
+                    }   //end else
+
+                default:
+                    log.Fatal(err);
+                }   //end switch
+            }   //end if
+
+            prog_a.IncrementInstructionIndex(was_jump);
+        }   //end for
 
     return;
 }   //end func
@@ -195,8 +257,7 @@ func NewProgram(new_registers map[string]int, new_instruction_idx int, receiving
 
 // shows whether the program can execute its current instruction
 func (program *Program) IsTerminated(instruction_count int) bool {
-    return ((program.cur_instruction_idx < 0) &&
-            (program.cur_instruction_idx >= instruction_count));
+    return ((program.cur_instruction_idx < 0) || (program.cur_instruction_idx >= instruction_count));
 }   //end func
 
 
@@ -225,10 +286,10 @@ func (program *Program) HandleInstruction(instruction *Instruction) (was_jump bo
         case "jgz":
             // if the first argument is greater than '0'
             if get_arg_value(instruction.args[0], program.registers) > 0 {
-                // add the offset to the current instruction index
-                program.cur_instruction_idx += get_arg_value(instruction.args[1], program.registers);
                 // set the flag to true
                 was_jump = true;
+                // add the offset to the current instruction index
+                program.cur_instruction_idx += get_arg_value(instruction.args[1], program.registers);
             }   //end if
 
         // any other instruction
@@ -264,6 +325,20 @@ func get_arg_value(argument string, registers map[string]int) (arg_value int) {
         // the value is the value of the given register name
         arg_value = registers[argument];
     }   //end if
+
+    return;
+}   //end func
+
+
+// returns a clone of the given int array
+func clone_int_array(array_to_clone []int) (cloned_array []int) {
+    // make an array of the same length
+    cloned_array = make([]int, len(array_to_clone));
+
+    // transfer all elements from the initial array
+    for i, value := range array_to_clone {
+        cloned_array[i] = value;
+    }   //end for
 
     return;
 }   //end func
