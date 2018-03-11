a0629e6b5296b4ffd1ea87d412a966d5f3c42007
vilskin <slavov.vili@gmail.com>
2018-03-11 17:55:58 +0100

    Advanced in day 19 - part 1

--- day_19/packet_rescuer.go
+++ day_19/packet_rescuer.go
@@ -5,6 +5,7 @@ import (
     "fmt"
     "log"
     "os"
+    "regexp"
     "strings"
 )   //end imports
 
@@ -13,6 +14,8 @@ func main() {
     var args = os.Args;
     // stores the matrix representing the path of the packet
     var packet_path = make([][]string, 0);
+    // stores the starting position of the path
+    var start_pos = 0;
 
     // open the file given as an argument
     file, err := os.Open(args[1]);
@@ -29,14 +32,197 @@ func main() {
         packet_path = append(packet_path, strings.Split(scanner.Text(), ""));
     }   //end for
 
-    // after the matrix has been filled in, store its sizes for ease of access
-    var path_x = len(packet_path);
-    var path_y = len(packet_path[0]);
+    // find the starting position
+    // for each character in the first line
+    for i, char := range packet_path[0] {
+        if char == "|" {
+            start_pos = i;
+            break;
+        }   //end if
+    }   //end for
+    
+    // create a packet carrier for the current path
+    var packet_carrier = NewPacketCarrier(NewPair(0, start_pos), "south")
 
 
     // PART I
+    // stores all the letters encountered while going through the path
+    var letters string;
+    // stores the character in the position in the path after the move
+    var next_char string;
+    // stores whether the packet has reached the end of the path
+    var at_end bool;
+    // a regexp to check if a string is a letter
+    var isLetter = regexp.MustCompile("[a-zA-Z]");
+
+    // while the packet hasn't reached the end of the path
+    for !at_end {
+        // move the packet one step and get the character at that position
+        next_char, at_end = packet_carrier.Move(packet_path);
+
+        // if the next character is a letter, store it to the result
+        if isLetter.MatchString(next_char) {
+            letters += next_char;
+        }   //end if
+    }   //end for
+
+    fmt.Println("Letters found:", letters);
+
+
+    // PART II
 
 }   //end main
+
+
+
+
+
+type PacketCarrier struct {
+    direction  string
+    cur_pos        *Pair
+}   //end type
+
+
+func NewPacketCarrier(new_pos *Pair, new_direction string) *PacketCarrier {
+    var new_packet_carrier = new(PacketCarrier);
+
+    new_packet_carrier.direction = new_direction;
+    new_packet_carrier.cur_pos = NewPair(new_pos.x, new_pos.y);
+    return new_packet_carrier;
+}   //end func
+
+
+// attempts to move the packet carrier in the given direction
+func (packet_carrier *PacketCarrier) Move(path [][]string) (next_char string, at_end bool) {
+    // store the character at the current position of the carrier
+    var cur_char = path[packet_carrier.cur_pos.x][packet_carrier.cur_pos.y];
+    fmt.Println("Moving from:", cur_char)
+
+
+    // stores whether the carrier is currently standing at a crossroad
+    var at_crossroad bool;
+    if cur_char == "+" {
+        at_crossroad = true;
+    } else {
+        at_crossroad = false;
+    }   //end else
+
+
+    // get the new position and the new direction of the carrier after the move
+    var new_pos, new_direction = get_new_pos(packet_carrier.cur_pos, packet_carrier.direction, at_crossroad, path);
+
+
+    // if we have reached the end of the path
+    if new_direction == "" {
+        at_end = true;
+    } else {
+        // move   to the new position
+        packet_carrier.cur_pos   = new_pos;
+        // adjust to the new direction
+        packet_carrier.direction = new_direction;
+
+        // store the character at the new position
+        next_char = path[new_pos.x][new_pos.y];
+    }   //end else
+    return;
+}   //end func
+
+
+
+// gets the new position and the new direction of the carrier
+func get_new_pos(cur_pos *Pair, cur_direction string, at_crossroad bool, path [][]string) (new_pos *Pair, new_direction string) {
+    // get a direction, which presents a possible road to continue
+    new_direction = get_next_dir(cur_pos, cur_direction, at_crossroad, path);
+
+    // if the direction exists (means we are NOT at the end of the path)
+    if new_direction != "" {
+        // calculate the next position based on the direction
+        new_pos = calc_new_pos(cur_pos, new_direction);
+    }   //end else
+    return;
+}   //end func
+
+
+
+// returns the direction in which the next road is found
+func get_next_dir(cur_pos *Pair, cur_direction string, at_crossroad bool, path [][]string) (new_direction string) {
+    // if we're NOT at a crossroad
+    if !at_crossroad {
+        // continue in the same direction
+        new_direction = cur_direction;
+
+    // if we ARE at a crossroad
+    } else {
+        // store a list of all possible directions
+        var all_directions = []string{"north","east","south","west"};
+
+        // for each direction in the list
+        for _, dir := range all_directions {
+            // if the direction is NOT the one where we came from
+            if dir != get_opposite_direction(cur_direction) {
+                // calculate the next position in that direction
+                var new_pos = calc_new_pos(cur_pos, dir);
+
+                // if the position is a valid coordinate in the path
+                if new_pos.is_valid_coord(len(path), len(path[0])) {
+                    // if the character at that position is NOT an empty space
+                    // there must be a road there
+                    if path[new_pos.x][new_pos.y] != " " {
+                        // store the direction
+                        new_direction = dir;
+                        break;
+                    }   //end if
+                }   //end if
+            }   //end if
+        }   //end for
+    }   //end else
+
+    return;
+}   //end func
+
+
+
+// calculates the next position in a given direction
+func calc_new_pos(cur_pos *Pair, direction string) (new_pos *Pair) {
+    switch direction {
+    // if we are moving north
+    case "north":
+        new_pos = NewPair((cur_pos.x - 1), cur_pos.y);
+
+    // if we are moving east
+    case "east":
+        new_pos = NewPair(cur_pos.x, (cur_pos.y + 1));
+
+    // if we are moving south
+    case "south":
+        new_pos = NewPair((cur_pos.x + 1), cur_pos.y);
+
+    // if we are moving west
+    case "west":
+        new_pos = NewPair(cur_pos.x, (cur_pos.y - 1));
+    }   //end switch
+    return;
+}   //end func
+
+
+
+// returns the opposite direction of the given one
+func get_opposite_direction(cur_direction string) (opposite_direction string) {
+    switch cur_direction {
+    case "north":
+        opposite_direction = "south";
+
+    case "east":
+        opposite_direction = "west";
+
+    case "south":
+        opposite_direction = "north";
+
+    case "west":
+        opposite_direction = "east";
+    }   //end switch
+    return;
+}   //end func
 
 
 
