50be823a4eb0ddd2ffb448a77798ad47cf00ff54
vilskin <slavov.vili@gmail.com>
2018-03-16 17:14:19 +0100

    Added some functionality to patterns

--- day_21/fractal_artist.go
+++ day_21/fractal_artist.go
@@ -36,8 +36,10 @@ func main() {
         // PART I
         fmt.Println("Starting Pattern:");
         fmt.Println(starting_pattern);
-        fmt.Println("Starting Rule:")
-        fmt.Println(RuleFromPattern(starting_pattern));
+        fmt.Println("Pattern flipped X:");
+        fmt.Println(starting_pattern.GetFlipX());
+        fmt.Println("Pattern flipped Y:");
+        fmt.Println(starting_pattern.GetFlipY());
 
 
         // PART II
@@ -48,6 +50,13 @@ func main() {
 }   //end main
 
 
+// TODO: implement function to find the rule which matches the given pattern
+// applies the rule which matches the given pattern and returns the result
+func match_rule_by_pattern(pattern *Pattern, rules map[string]string) string {
+    return "";
+}   //end func
+
+
 
 
 
@@ -55,24 +64,28 @@ func main() {
 type Pattern [][]string
 
 
-func (pattern Pattern) String() (pretty_pattern string) {
-    // for each line in the pattern
-    for _, line := range pattern {
-        pretty_pattern += fmt.Sprintf("%v\n", strings.Join(line, ""));
+func NewPattern(size int) *Pattern {
+    var new_pattern = new(Pattern);
+    *new_pattern = make([][]string, size);
+
+    // for each line in the new pattern
+    for i,_ := range *new_pattern {
+        (*new_pattern)[i] = make([]string, size);
     }   //end for
-    return;
+    return new_pattern;
 }   //end func
 
 
 // return a pattern, created from the given rule
 func PatternFromRule(rule string) *Pattern {
-    var pattern = new(Pattern);
-
     // split the rule into the lines of the pattern
     var lines = strings.Split(rule, "/");
-    for _, line := range lines {
+    // create a new pattern
+    var pattern = NewPattern(len(lines));
+
+    for i, line := range lines {
         // split the line into its characters and add it to the pattern
-        *pattern = append(*pattern, strings.Split(line, ""));
+        (*pattern)[i] = strings.Split(line, "");
     }   //end for
     return pattern;
 }   //end func
@@ -88,6 +101,63 @@ func RuleFromPattern(pattern *Pattern) (rule string) {
             format_string += "/";
         }   //end if
         rule += fmt.Sprintf(format_string, strings.Join(line, ""));
+    }   //end for
+    return;
+}   //end func
+
+
+// returns the result of flipping the pattern along the X axis
+func (pattern Pattern) GetFlipX() *Pattern {
+    var pattern_size = len(pattern);
+    var pattern_flipped = NewPattern(pattern_size);
+
+    // for each line in the pattern
+    for i, line := range pattern {
+        // for each character in the line
+        for j, char := range line {
+            (*pattern_flipped)[pattern_size-i-1][j] = char;
+        }   //end for
+    }   //end for
+
+    return pattern_flipped;
+}   //end func
+
+
+// returns the result of flipping the pattern along the Y axis
+func (pattern Pattern) GetFlipY() *Pattern {
+    var pattern_size = len(pattern);
+    var pattern_flipped = NewPattern(pattern_size);
+
+    // for each line of the pattern
+    for i, line := range pattern {
+        // for each character in the line
+        for j, char := range line {
+            (*pattern_flipped)[i][pattern_size-j-1] = char;
+        }   //end for
+    }   //end for
+
+    return pattern_flipped;
+}   //end func
+
+
+// returns the result of rotating the give pattern by 90 degrees
+func (pattern Pattern) GetRotate90() *Pattern {
+    var pattern_size = len(pattern);
+    var pattern_rotated = NewPattern(pattern_size);
+
+
+
+    return pattern_rotated;
+}   //end func
+
+
+// TODO: implement function to split Pattern in smaller chunks
+
+
+func (pattern Pattern) String() (pretty_pattern string) {
+    // for each line in the pattern
+    for _, line := range pattern {
+        pretty_pattern += fmt.Sprintf("%v\n", strings.Join(line, ""));
     }   //end for
     return;
 }   //end func
