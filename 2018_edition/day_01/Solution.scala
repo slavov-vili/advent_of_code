47d4e0ee6b2ddbd15d5c21a7595a525fefd2935e
vilskin <slavov.vili@gmail.com>
2018-12-02 10:51:30 +0100

    Fixed day 1 solutions for efficiency

--- 2018_edition/day_01/Solution.scala
+++ 2018_edition/day_01/Solution.scala
@@ -6,7 +6,7 @@ println("Solution to A: " + solveA(input, 0))
 println("Solution to A(alt): " + solveA_alt(input))
 
 println("Solution to B: " + solveB(input, Set(), 0))
-println("Solution to B(alt): " + solveB_alt(input, Set(), 0))
+println("Solution to B(alt): " + solveB_alt(input, Set(0), 0))
 
 
 
@@ -25,21 +25,6 @@ def solveA_alt(input: List[Int]): Int = {
 def solveB(input: List[Int], seenFreqs: Set[Int],
     initialFreq: Int): Int = {
 
-  def solveB_internal(curInput: List[Int], curSeenFreqs: Set[Int],
-      curFreq: Int): (Int, Option[Set[Int]]) = {
-    curSeenFreqs.contains(curFreq) match {
-      case true => {
-        (curFreq, None)
-      }
-      case false => {
-        curInput match {
-          case x :: tail => solveB_internal(tail, curSeenFreqs + curFreq, curFreq + x)
-          case Nil => (curFreq, Some(curSeenFreqs))
-        }
-      }
-    }
-  }
-
   val (curFreq, curSeenFreqs) = solveB_internal(input, seenFreqs, initialFreq)
   curSeenFreqs match {
     case Some(x) => solveB(input, x, curFreq)
@@ -47,10 +32,25 @@ def solveB(input: List[Int], seenFreqs: Set[Int],
   }
 }
 
+def solveB_internal(curInput: List[Int], curSeenFreqs: Set[Int],
+    curFreq: Int): (Int, Option[Set[Int]]) = {
+  curSeenFreqs.contains(curFreq) match {
+    case true => {
+      (curFreq, None)
+    }
+    case false => {
+      curInput match {
+        case x :: tail => solveB_internal(tail, curSeenFreqs + curFreq, curFreq + x)
+        case Nil => (curFreq, Some(curSeenFreqs))
+      }
+    }
+  }
+}
+
 
 def solveB_alt(input: List[Int], prevSums: Set[Int],
     initialFreq: Int): Int = {
-  val newSums = input.scanLeft(initialFreq)(_ + _)
+  val newSums = input.scanLeft(initialFreq)(_ + _).tail
   val firstDuplicate = newSums.find(x => prevSums.contains(x))
 
   firstDuplicate match {
@@ -59,7 +59,7 @@ def solveB_alt(input: List[Int], prevSums: Set[Int],
       // Exclude first element, because else it will be there twice
       // - Once because it was calculated last call
       // - Another time because scanLeft adds it this call as well
-      val combinedSums = prevSums ++ newSums.init
+      val combinedSums = prevSums ++ newSums
       solveB_alt(input, combinedSums, newSums.last)
     }
   }
