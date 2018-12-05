6953bfd8b17af55d5de1693b5712169fbc793257
vilskin <slavov.vili@gmail.com>
2018-12-05 19:19:19 +0100

    Refactored solutions to day 5

--- 2018_edition/day_05/Solution.scala
+++ 2018_edition/day_05/Solution.scala
@@ -2,42 +2,24 @@ import scala.io.Source
 
 val input = readInput("input.txt")
 
-// TODO: think of alt version where it just takes tuples or make recursion more efficient
-// println("Solution to A: " + solveA(input.toList))
+println("Solution to A: " + solveA(input))
 
-println("Solution to B: " + solveB(input.toList))
-
+println("Solution to B: " + solveB(input))
 
 
-// NOTE: it's EXTREMELY slow due to either too large input for such recursion
-//       OR (more likely) -> inefficient recursion
-// NOTE: produces a stackoverflow, so you need to make the stack bigger (scala -J-Xss200m)
-def solveA(input: List[Char]): Int = {
-  return reducePolymer(input).size
+
+def solveA(input: String): Int = {
+  return input.toList
+              .foldLeft(List[Char]())(reducePolymerOnce).size
 }
 
 
-def reducePolymer(polymer: List[Char]): List[Char] =  {
-  val newPolymer = reducePolymer_helper(polymer, ' ')
-  // println("Polymer size: " + polymer.size)
-
-  return if(polymer.size == newPolymer.size) newPolymer
-         else reducePolymer(newPolymer)
-}
-
-
-def reducePolymer_helper(polymer: List[Char], lastChar: Char): List[Char] = {
-  val newPolymer = polymer match {
-                     case Nil => List(lastChar)
-                     case head :: tail => {
-                       if(checkIfReact(lastChar, head)) {
-                         // println("Reduced: " + lastChar + " & " + head)
-                         reducePolymer_helper(tail, ' ')
-                       }
-                       else lastChar :: reducePolymer_helper(tail, head)
-                     }
-                   }
-  return newPolymer.filterNot(x => x == ' ')
+def reducePolymerOnce(polymer: List[Char], unit: Char): List[Char] = {
+  return polymer.headOption match {
+          case Some(head) => if(checkIfReact(head, unit)) polymer.tail
+                             else unit :: polymer
+          case None => List(unit)
+         }
 }
 
 
@@ -48,23 +30,17 @@ def checkIfReact(char1: Char, char2: Char): Boolean = {
 
 
 
-// NOTE: it's EXTREMELY slow due to either too large input for such recursion
-//       OR (more likely) -> inefficient recursion
-// NOTE: produces a stackoverflow, so you need to make the stack bigger (scala -J-Xss200m)
-def solveB(input: List[Char]): Int = {
-  val units = input.map(_.toString.toLowerCase()(0)).toSet
-    
-  val unitToEffect = units.map(x => (x, solveA(removeUnit(input, x))))
+def solveB(input: String): Int = {
+  val units = input.toLowerCase
+                   .toSet
 
-  unitToEffect.foreach(x => println(x._1 + " -> " + x._2))
-  
-  return unitToEffect.maxBy(_._2)
-                     ._2
+  return units.map(x => solveA(removeUnit(input.toList, x).mkString))
+              .min
 }
 
 
 def removeUnit(polymer: List[Char], unitLowerCase: Char): List[Char] = {
-  return polymer.filterNot(x => x.toString.toLowerCase()(0) == unitLowerCase)
+  return polymer.filterNot(x => x.toString.toLowerCase() == unitLowerCase.toString)
 }
 
 
