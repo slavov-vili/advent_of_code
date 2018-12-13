9ba6f2e8709cc2c6fe5ca87818c3a58555906349
vilskin <slavov.vili@gmail.com>
2018-12-13 07:30:35 +0100

    Added solution to day 11, part 2

--- 2018_edition/day_11/Solution.scala
+++ 2018_edition/day_11/Solution.scala
@@ -5,38 +5,57 @@ val gridSize = 300
 val input = 3613
 val grid = generateGrid(startCoords, gridSize, input)
 //println(drawGrid(grid))
+val initialGridMetaData = new GridMetaData(startCoords, 3, Int.MinValue)
 
 // TODO: REMEMBER to add 1 to the coordinates and reverse X and Y when giving output
-val solutionMax3x3 = solveA(startCoords, grid, startCoords, Int.MinValue)
-println("Solution to A: " + solutionMax3x3)
+println("Solution to A: " + solveA(startCoords, grid, initialGridMetaData))
+
+println("Solution to B: " + solveB(startCoords, 1, grid, initialGridMetaData))
 
 
-def solveA(curCoords: Pair, grid: Vector[Vector[Int]], maxGridCoords: Pair, curMaxPowerLevel: Int): (Pair, Int) = {
-  return if(curCoords.getX == grid.size-2) (maxGridCoords, curMaxPowerLevel)
+def solveA(curCoords: Pair, grid: Vector[Vector[Int]], curMaxGridMetaData: GridMetaData): GridMetaData = {
+  val curGridSize = curMaxGridMetaData.getSize
+  return if(curCoords.getX == grid.size-(curGridSize-1)) curMaxGridMetaData
           else {
-            val (newMaxGridCoords, newMaxPowerLevel) = findCurMaxGridStart(curCoords, grid, maxGridCoords, curMaxPowerLevel)
-            solveA(curCoords + new Pair(1, 0), grid, newMaxGridCoords, newMaxPowerLevel)
+            val newMaxGridMetaData = findCurMaxGrid(curCoords, grid, curMaxGridMetaData)
+            solveA(curCoords + new Pair(1, 0), grid, newMaxGridMetaData)
           }
 }
 
 
-def findCurMaxGridStart(curCoords: Pair, grid: Vector[Vector[Int]], curMaxGridCoords: Pair,
-                        curMaxPowerLevel: Int): (Pair, Int) = {
+// TODO: iterate until power level starts getting smaller/negative
+def solveB(curCoords: Pair, curGridSize: Int, grid: Vector[Vector[Int]], curMaxGridMetaData: GridMetaData): GridMetaData = {
+  val curSizeMaxGridMetaData = solveA(curCoords, grid, new GridMetaData(curCoords, curGridSize, Int.MinValue))
+  println("Current grid size: " + curGridSize)
+  println("Current Max Grid MetaData: " + curMaxGridMetaData)
+  println("Current Size Max Grid MetaData: " + curSizeMaxGridMetaData)
+
+  return if(curSizeMaxGridMetaData.getPowerLevel < 0)
+            curMaxGridMetaData
+         else solveB(curCoords, curGridSize+1, grid,
+                     curMaxGridMetaData.getMorePowerful(curSizeMaxGridMetaData))
+}
+
+
+def findCurMaxGrid(curCoords: Pair, grid: Vector[Vector[Int]], curMaxGridMetaData: GridMetaData): GridMetaData = {
   //println("Current Coords: " + curCoords)
   //println("Current max power level Coords: " + curMaxGridCoords)
   //println("Current max power level: " + curMaxPowerLevel)
   val x = curCoords.getX
+  val curGridSize = curMaxGridMetaData.getSize
   // Calculate the power levels of all 3x3 grids in the current 3x3 row
-  val gridStartToPowerLevel = Range(curCoords.getY, grid.head.size-2).map(y => {
-          val curGrid = grid.slice(x, x+3).map(_.slice(y, y+3))
+  val gridStartToPowerLevel = Range(curCoords.getY, grid.head.size-(curGridSize-1)).map(y => {
+          val curGrid = grid.slice(x, x+curGridSize).map(_.slice(y, y+curGridSize))
           //println("Current Grid to Sum: " + new Pair(x, y) + " " + curGrid + " " + calcGridPowerLevel(curGrid))
           (new Pair(x, y), calcGridPowerLevel(curGrid))
       })
-  val maxGridToPowerLevelInList = gridStartToPowerLevel.maxBy(x => x._2)
+  // the maximum power level found in the current list of 3x3 grids
+  val maxGridStartToPowerLevel = gridStartToPowerLevel.maxBy(x => x._2)
   //if(maxGridToPowerLevelInList._2 > curMaxPowerLevel)
     //println("Bigger in list than max: " + maxGridToPowerLevelInList)
-  return if(maxGridToPowerLevelInList._2 > curMaxPowerLevel) maxGridToPowerLevelInList
-         else (curMaxGridCoords, curMaxPowerLevel)
+  return if(maxGridStartToPowerLevel._2 > curMaxGridMetaData.getPowerLevel)
+            new GridMetaData(maxGridStartToPowerLevel._1, curGridSize, maxGridStartToPowerLevel._2)
+         else new GridMetaData(curMaxGridMetaData.getStartCoords, curGridSize, curMaxGridMetaData.getPowerLevel)
 }
 
 
@@ -62,6 +81,21 @@ def drawGrid(grid: Vector[Vector[Int]]): String = {
 
 
 
+
+
+class GridMetaData(startCoords: Pair, size: Int, powerLevel: Int) {
+  def getStartCoords(): Pair = this.startCoords;
+  def getSize(): Int = this.size;
+  def getPowerLevel(): Int = this.powerLevel
+
+  def getMorePowerful(other: GridMetaData): GridMetaData = {
+    return if(this.powerLevel > other.getPowerLevel) this
+           else other
+  }
+
+  override def toString(): String = "(" + this.startCoords + " : Size = " +
+                                    this.size + ", PL = " + this.powerLevel + ")"
+}
 
 
 object PowerCell {
