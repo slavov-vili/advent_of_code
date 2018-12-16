937afb0d508abebe2132e8fac5d9dc09824c685c
vilskin <slavov.vili@gmail.com>
2018-12-16 19:54:22 +0100

    Added solution to day 12 part 2

--- 2018_edition/day_12/Solution.scala
+++ 2018_edition/day_12/Solution.scala
@@ -2,19 +2,18 @@ import scala.io.Source
 
 
 
-val (initialTunnel, rules) = readInput("input.txt")
-val generationCountA = 20
-
 val POT_CHAR_EMPTY = '.'
 val POT_CHAR_FULL  = '#'
 
+val (initialTunnel, rules) = readInput("input.txt", POT_CHAR_EMPTY, 5)
+val generationCountA = 20
+val generationCountB = 50000000000L
 
-val endTunnel = solveA(initialTunnel, rules, generationCountA, POT_CHAR_EMPTY)
-println("Solution to A: " + endTunnel.getState
-                                     .zipWithIndex
-                                     .filter(_._1 == POT_CHAR_FULL)
-                                     .map(_._2 - endTunnel.getIndex0)
-                                     .sum)
+
+val endTunnelA = solveA(initialTunnel, rules, generationCountA, POT_CHAR_EMPTY)
+println("Solution to A: " + endTunnelA.calcPotScore(POT_CHAR_FULL))
+val endScoreB = solveB(initialTunnel, rules, Map[String, PotTunnel](), generationCountB, POT_CHAR_EMPTY, POT_CHAR_FULL)
+println("Solution to B: " + endScoreB)
 
 
 
@@ -29,35 +28,57 @@ def solveA(curTunnel: PotTunnel, rules: Map[String, Char], remainingGenerationCo
 }
 
 
-
-
-
-def readInput(filename: String): (PotTunnel, Map[String, Char]) = {
-  val lines = Source.fromFile(filename)
-                    .getLines
-                    .toList
-  val initialState = lines.head
-                          .split(" ")
-                          .apply(2)
-  val rules = lines.tail
-                   .tail
-                   .map(line => line.split(" => "))
-                   .map(rule => (rule(0), rule(1).head))
-                   .toMap
-  return (new PotTunnel(0, initialState), rules)
+def solveB(curTunnel: PotTunnel, rules: Map[String, Char], seenStates: Map[String, PotTunnel],
+           generationCount: Long, emptyPotChar: Char, fullPotChar: Char): Long = {
+  println("Seen states: " + seenStates.size)
+  val (prefixRemoved, curTunnelStateTrimmed, suffixRemoved) = PotTunnel.trimState(curTunnel.getState, emptyPotChar)
+  return seenStates.contains(curTunnelStateTrimmed) match {
+    case false => solveB(curTunnel.spread(rules, emptyPotChar), rules,
+                         seenStates + (curTunnelStateTrimmed -> curTunnel),
+                         generationCount, emptyPotChar, fullPotChar)
+    case true => {
+      val curStateScore = curTunnel.calcPotScore(fullPotChar)
+      val repeatedStateScore = seenStates(curTunnelStateTrimmed).calcPotScore(fullPotChar)
+      //println("Repeating pattern (trimmed): " + curTunnelStateTrimmed)
+      //println("Repeating State: " + seenStates(curTunnelStateTrimmed))
+       curStateScore.toLong + ((generationCount - seenStates.size) * (curStateScore - repeatedStateScore)).toLong
+    }
+  }
 }
 
 
 
 
 
-class PotTunnel(index0: Int, state: String) {
+def readInput(filename: String, emptyPotChar: Char, paddingSize: Int): (PotTunnel, Map[String, Char]) = {
+  val lines = Source.fromFile(filename)
+                    .getLines
+                    .toList
+  val initialState = lines.head
+                          .split(" ")
+                          .apply(2)
+  // put padding on the initial state
+  val (prefixAdded, initialStatePadded, suffixAdded) = PotTunnel.padState(initialState, emptyPotChar, paddingSize)
+  val rules = lines.tail
+                   .tail
+                   .map(line => line.split(" => "))
+                   .map(rule => (rule(0), rule(1).head))
+                   .toMap
+  return (new PotTunnel(prefixAdded, initialStatePadded, paddingSize), rules)
+}
+
+
+
+
+
+class PotTunnel(index0: Int, state: String, paddingSize: Int) {
   def getIndex0(): Int = this.index0;
   def getState(): String = this.state;
+  def getPaddingSize(): Int = this.paddingSize;
 
   // Pads the string with dots before spreading, which results in different input
   def spread(rules: Map[String, Char], emptyPotChar: Char): PotTunnel = {
-    val (prefixAdded, statePadded, suffixAdded) = PotTunnel.padString(state, emptyPotChar, 5)
+    val (prefixAdded, statePadded, suffixAdded) = PotTunnel.padState(state, emptyPotChar, paddingSize)
 
     val newState = Range(0, statePadded.size).map(i => {
       val LLCRR = statePadded.slice(i-2, i+3).mkString("")
@@ -65,14 +86,23 @@ class PotTunnel(index0: Int, state: String) {
     }).mkString("")
 
     val newIndex0 = index0 + prefixAdded
-    return new PotTunnel(newIndex0, newState)
+    return new PotTunnel(newIndex0, newState, this.paddingSize)
+  }
+
+  def calcPotScore(fullPotChar: Char): Int = {
+    return this.state
+               .zipWithIndex
+               .filter(_._1 == fullPotChar)
+               .map(_._2 - this.index0)
+               .sum
   }
 
   override def toString(): String = "(" + this.index0 + ", " + this.state.toString + ")"
 
   override def equals(obj: Any): Boolean = obj match {
     case obj: PotTunnel => this.index0 == obj.getIndex0 &&
-                           this.state == obj.getState
+                           this.state == obj.getState &&
+                           this.paddingSize == obj.getPaddingSize
     case _ => false
   }
 
@@ -82,12 +112,12 @@ class PotTunnel(index0: Int, state: String) {
 object PotTunnel {
   // Pads a string with 5 dots on each side
   // returns the new string as well as how many new dots were added
-  def padString(oldString: String, charToPad: Char, paddingSize: Int): (Int, String , Int) = {
-    val prefixDotsCount = oldString.takeWhile(_ == charToPad)
-                                   .size
-    val suffixDotsCount = oldString.reverse
-                                   .takeWhile(_ == charToPad)
-                                   .size
+  def padState(oldState: String, charToPad: Char, paddingSize: Int): (Int, String , Int) = {
+    val prefixDotsCount = oldState.takeWhile(_ == charToPad)
+                                  .size
+    val suffixDotsCount = oldState.reverse
+                                  .takeWhile(_ == charToPad)
+                                  .size
 
     val prefixToAdd = if(prefixDotsCount < paddingSize) (paddingSize - prefixDotsCount)
                       else 0
@@ -95,7 +125,21 @@ object PotTunnel {
                       else 0
 
     return (prefixToAdd,
-            ((charToPad.toString * prefixToAdd) + oldString + (charToPad.toString) * suffixToAdd),
+            ((charToPad.toString * prefixToAdd) + oldState + (charToPad.toString * suffixToAdd)),
             suffixToAdd)
+  }
+
+
+  def trimState(oldState: String, charToTrim: Char): (Int, String, Int) = {
+    val prefixDotsCount = oldState.takeWhile(_ == charToTrim)
+                                  .size
+    val suffixDotsCount = oldState.reverse
+                                  .takeWhile(_ == charToTrim)
+                                  .size
+
+    return (prefixDotsCount,
+            oldState.take(oldState.length - suffixDotsCount)
+                    .drop(prefixDotsCount),
+            suffixDotsCount)
   }
 }
