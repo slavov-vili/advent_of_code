59d5ee5c62bd91592293176d201f593a388a0d30
vilskin <slavov.vili@gmail.com>
2019-01-22 07:44:45 +0100

    Added summary of rules for day 15 part 1

--- 2018_edition/day_15/Solution.scala
+++ 2018_edition/day_15/Solution.scala
@@ -1,5 +1,36 @@
 import scala.io.Source
 
+// # = wall
+// . = space
+// G = Goblin
+// E = Elf
+// game = [round]
+// round = aliveUnits.foreach(takeTurn)
+// turn order = reading order at beginning of round
+//
+// turn = move || attack (no diagonals)
+//        if(inRange) attack
+//        if(existsTarget) move
+//        else endTurn
+//
+// attack = find closest target with lowest health, attack it
+//          if (len(reachableTargets) == 0) endTurn
+//          if (len(closest) > 1) getFirstInReadingOrder
+//          if (len(mostDamaged) > 1) getFirstInReadingOrder
+//
+//
+// move = find closest open square, move towards it
+//        (cannot pass through walls or other units)
+//        if (len(closest) > 1) getFirstInReadingOrder
+//        if (len(nextStep) > 1) getFirstInReadingOrder
+//
+// startDamage = 3
+// startHealth = 200
+// if (health <= 0) dead
+// if (dead) square = .
+//
+// FIND: countTurns * totalHP left
+
 
 
 val input = readInput("input.txt")
