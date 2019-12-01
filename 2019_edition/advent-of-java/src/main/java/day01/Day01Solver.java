92c84dbbcd5dbb4a7839227c58be3f7297beecc0
Velislav Slavov <slavov.vili@gmail.com>
2019-12-01 10:19:43 +0100

    Fixed method names in day 01 solver

--- 2019_edition/advent-of-java/src/main/java/day01/Day01Solver.java
+++ 2019_edition/advent-of-java/src/main/java/day01/Day01Solver.java
@@ -3,38 +3,47 @@ package day01;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import utils.AdventOfCodeUtils;
+
 public class Day01Solver {
 
     public int solveA(List<Integer> moduleMasses) {
-	return calcFuelCostForAllModuleMasses(moduleMasses).stream()
-		.reduce(0, (a, b) -> a + b);
+	return AdventOfCodeUtils.sumListOfIntegers(calcSimplifiedFuelCostForAllModuleMasses(moduleMasses));
     }
 
-    protected static List<Integer> calcFuelCostForAllModuleMasses(List<Integer> moduleMasses) {
+    protected static List<Integer> calcSimplifiedFuelCostForAllModuleMasses(List<Integer> moduleMasses) {
 	return moduleMasses.stream()
-		.map(mass -> calcFuelCostOfModule(mass))
+		.map(mass -> calcSimplifiedFuelCostOfModule(mass))
 		.collect(Collectors.toList());
     }
 
-    protected static int calcFuelCostOfModule(int moduleMass) {
+    protected static int calcSimplifiedFuelCostOfModule(int moduleMass) {
 	return (Math.floorDiv(moduleMass, 3) - 2);
     }
 
     
     
     public int solveB(List<Integer> moduleMasses) {
-	List<Integer> fuelMasses = calcFuelCostForAllModuleMasses(moduleMasses);
-
-	return fuelMasses.stream()
-		.map(fuelMass -> calcFuelCostOfFuel(fuelMass))
-		.reduce(0, (a, b) -> a + b);
+	return AdventOfCodeUtils.sumListOfIntegers(calcAdvancedFuelCostForAllModuleMasses(moduleMasses));
+    }
+    
+    protected static List<Integer> calcAdvancedFuelCostForAllModuleMasses(List<Integer> moduleMasses) {
+	return moduleMasses.stream()
+		.map(mass -> calcAdvancedFueldCostOfModule(mass))
+		.collect(Collectors.toList());
+    }
+    
+    protected static int calcAdvancedFueldCostOfModule(int moduleMass) {
+	int simplifiedFuelCost = calcSimplifiedFuelCostOfModule(moduleMass);
+	
+	return calcFuelCostOfFuel(simplifiedFuelCost);
     }
 
     protected static int calcFuelCostOfFuel(int fuelMass) {
 	if (fuelMass <= 0) {
 	    return 0;
 	}
-
-	return fuelMass + calcFuelCostOfFuel(calcFuelCostOfModule(fuelMass));
+	
+	return fuelMass + calcFuelCostOfFuel(calcSimplifiedFuelCostOfModule(fuelMass));
     }
 }
--- 2019_edition/advent-of-java/src/test/java/day01/Day01Test.java
+++ 2019_edition/advent-of-java/src/test/java/day01/Day01Test.java
@@ -8,59 +8,63 @@ public class Day01Test {
 
     
     @Test
-    void calcFuelForMass_BasicTest() {
+    void calcSimplifiedFuelForMass_BasicTest() {
 	int expected = 2;
 	int inputMass = 12;
-	int actual = Day01Solver.calcFuelCostOfModule(inputMass);
+	int actual = Day01Solver.calcSimplifiedFuelCostOfModule(inputMass);
 	assertEquals(expected, actual, "A module mass of " + inputMass + " should yield a fuel cost of " + expected);
     }
     
     @Test
-    void calcFuelForMass_RoundingTest() {
+    void calcSimplifiedFuelForMass_RoundingTest() {
 	int expected = 2;
 	int inputMass = 14;
-	int actual = Day01Solver.calcFuelCostOfModule(inputMass);
+	int actual = Day01Solver.calcSimplifiedFuelCostOfModule(inputMass);
 	assertEquals(expected, actual, "A module mass of " + inputMass + " should yield a fuel cost of " + expected);
     }
     
     @Test
-    void calcFuelForMass_AdvancedTest() {
+    void calcSimplifiedFuelForMass_AdvancedTest() {
 	int expected = 654;
 	int inputMass = 1969;
-	int actual = Day01Solver.calcFuelCostOfModule(inputMass);
+	int actual = Day01Solver.calcSimplifiedFuelCostOfModule(inputMass);
 	assertEquals(expected, actual, "A module mass of " + inputMass + " should yield a fuel cost of " + expected);
     }
     
     @Test
-    void calcFuelForMass_ComplicatedTest() {
+    void calcSimplifiedFuelForMass_ComplicatedTest() {
 	int expected =33583;
 	int inputMass = 100756;
-	int actual = Day01Solver.calcFuelCostOfModule(inputMass);
+	int actual = Day01Solver.calcSimplifiedFuelCostOfModule(inputMass);
 	assertEquals(expected, actual, "A module mass of " + inputMass + " should yield a fuel cost of " + expected);
     }
     
+    
+    
+    
+    
     @Test
-    void calcFuelCostOfFuel_BasicTest() {
+    void calcAdvancedFuelCostOfFuel_BasicTest() {
 	int expected = 2;
-	int inputFuelMass = 2;
-	int actual = Day01Solver.calcFuelCostOfFuel(inputFuelMass);
-	assertEquals(expected, actual, "A fuel mass of " + inputFuelMass + " should yield a fuel cost of " + expected);
+	int inputMass = 14;
+	int actual = Day01Solver.calcAdvancedFueldCostOfModule(inputMass);
+	assertEquals(expected, actual, "A fuel mass of " + inputMass + " should yield a fuel cost of " + expected);
     }
     
     @Test
-    void calcFuelCostOfFuel_AdvancedTest() {
+    void calcAdvancedFuelCostOfFuel_AdvancedTest() {
 	int expected = 966;
-	int inputFuelMass = 654;
-	int actual = Day01Solver.calcFuelCostOfFuel(inputFuelMass);
-	assertEquals(expected, actual, "A fuel mass of " + inputFuelMass + " should yield a fuel cost of " + expected);
+	int inputMass = 1969;
+	int actual = Day01Solver.calcAdvancedFueldCostOfModule(inputMass);
+	assertEquals(expected, actual, "A fuel mass of " + inputMass + " should yield a fuel cost of " + expected);
     }    
     
     @Test
-    void calcFuelCostOfFuel_ComplicatedTest() {
+    void calcAdvancedFuelCostOfFuel_ComplicatedTest() {
 	int expected = 50346;
-	int inputFuelMass = 33583;
-	int actual = Day01Solver.calcFuelCostOfFuel(inputFuelMass);
-	assertEquals(expected, actual, "A fuel mass of " + inputFuelMass + " should yield a fuel cost of " + expected);
+	int inputMass = 100756;
+	int actual = Day01Solver.calcAdvancedFueldCostOfModule(inputMass);
+	assertEquals(expected, actual, "A fuel mass of " + inputMass + " should yield a fuel cost of " + expected);
     }
     
 }
