c5ea2afebd4aa1e93fd4c36c78593bb3a5ba9c98
Velislav Slavov <slavov.vili@gmail.com>
2021-06-15 14:20:21 +0200

    Got rid of the old day02 since refactoring is caught up

--- 2019_edition/advent-of-java/src/main/java/day02_old/instructions/NoSuchParameterException.java
+++
@@ -1,7 +0,0 @@
-package day02_old.instructions;
-
-public class NoSuchParameterException extends RuntimeException {
-    public NoSuchParameterException(String message) {
-        super(message);
-    }
-}
--- 2019_edition/advent-of-java/src/main/java/day02_old/instructions/UnknownParameterModeException.java
+++
@@ -1,7 +0,0 @@
-package day02_old.instructions;
-
-public class UnknownParameterModeException extends RuntimeException {
-    public UnknownParameterModeException(String message) {
-        super(message);
-    }
-}
