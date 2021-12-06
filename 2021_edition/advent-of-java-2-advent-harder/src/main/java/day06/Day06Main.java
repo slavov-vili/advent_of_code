b911ed6d0e5a503e7a4e4092487100cd335c0760
Velislav Slavov <velislav.slavov@cas.de>
2021-12-06 07:13:21 +0100

    Day 06 Part 2

--- 2021_edition/advent-of-java-2-advent-harder/src/main/java/day06/Day06Main.java
+++ 2021_edition/advent-of-java-2-advent-harder/src/main/java/day06/Day06Main.java
@@ -2,8 +2,9 @@ package day06;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import utils.AdventOfCodeUtils;
@@ -16,26 +17,60 @@ public class Day06Main {
 				.collect(Collectors.toList());
 
 		System.out.println("Fish after 80 days: " + solveA(fishAges));
+		
+		System.out.println("Fish after 256 days: " + solveB(fishAges));
 	}
 	
 	public static int solveA(List<Integer> fishAges) {
-		return simulateNDays(fishAges, 80).size();
+		return simulateNDaysA(fishAges, 80).size();
 	}
 	
-	public static List<Integer> simulateNDays(List<Integer> fishAges, int n) {
-		List<Integer> finalAges = new LinkedList<>(fishAges);
+	public static long solveB(List<Integer> fishAges) {
+		Map<Integer, Long> fishAgeCountMap = new HashMap<>();
+		fishAges.forEach(age -> updateCountMapBy(fishAgeCountMap, age, 1L));
+		
+		return simulateNDaysB(fishAgeCountMap, 256).entrySet().stream()
+				.mapToLong(Map.Entry::getValue).sum();
+	}
+	
+	public static List<Integer> simulateNDaysA(List<Integer> fishAges, int n) {
+		List<Integer> finalAges = new ArrayList<>(fishAges);
 		for (int i=0; i<n; i++) {
 			System.out.println("Day " + i);
-			finalAges = simulateDay(finalAges);
+			finalAges = simulateDayA(finalAges);
 		}
 		return finalAges;
 	}
 
-	public static List<Integer> simulateDay(List<Integer> fishAges) {
+	public static List<Integer> simulateDayA(List<Integer> fishAges) {
 		List<Integer> births = fishAges.stream().filter(age -> age == 0).map(age -> 8).collect(Collectors.toList());
 		List<Integer> nextAges = fishAges.stream().map(Day06Main::calcNextAge).collect(Collectors.toList());
 		nextAges.addAll(births);
 		return nextAges;
+	}
+	
+	public static Map<Integer, Long> simulateNDaysB(Map<Integer, Long> fishAgeCountMap, int n) {
+		Map<Integer, Long> finalFishAgeCountMap = new HashMap<>(fishAgeCountMap);
+		for (int i=0; i<n; i++) {
+			System.out.println("Day " + (i+1));
+			finalFishAgeCountMap = simulateDayB(finalFishAgeCountMap);
+		}
+		return finalFishAgeCountMap;
+	}
+	
+	public static Map<Integer, Long> simulateDayB(Map<Integer, Long> fishAgeCountMap) {
+		Map<Integer, Long> nextFishAgeCountMap = new HashMap<>(fishAgeCountMap);
+		long births = fishAgeCountMap.getOrDefault(0, 0L);
+		
+		for(int age : fishAgeCountMap.keySet()) {	
+			int newAge = calcNextAge(age);
+
+			updateCountMapBy(nextFishAgeCountMap, age, -fishAgeCountMap.getOrDefault(age, 0L));
+			updateCountMapBy(nextFishAgeCountMap, newAge, fishAgeCountMap.getOrDefault(age, 0L));
+		}
+		
+		updateCountMapBy(nextFishAgeCountMap, 8, births);
+		return nextFishAgeCountMap;
 	}
 	
 	public static int calcNextAge(int age) {
@@ -43,5 +78,10 @@ public class Day06Main {
 			return 6;
 		else
 			return age - 1;
+	}
+	
+	public static void updateCountMapBy(Map<Integer, Long> countMap, Integer keyToUpdate, long offset) {
+		Long newValue = countMap.getOrDefault(keyToUpdate, 0L) + offset;
+		countMap.put(keyToUpdate, newValue);
 	}
  }
