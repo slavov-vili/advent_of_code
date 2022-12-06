c42e500373a720225cb649bb1c1c1be5c89e3723
Velislav Slavov <velislav.slavov@cas.de>
2022-12-06 13:54:25 +0100

    AOC 2022: Add solution to day06

--- 2022_edition/advent-of-java-again/src/main/java/day06/Day06Main.java
+++ 2022_edition/advent-of-java-again/src/main/java/day06/Day06Main.java
@@ -1,18 +1,29 @@
 package day06;
 
-import java.util.List;
-
 import utils.AdventOfCodeUtils;
 
 public class Day06Main {
-
 	public static void main(String[] args) {
-		List<String> input = AdventOfCodeUtils.readInput(Day06Main.class);
+		String input = AdventOfCodeUtils.readInput(Day06Main.class).get(0);
+
+		System.out.println("Characters until packet marker: " + solveA(input));
+		System.out.println("Characters until message marker: " + solveB(input));
 	}
 
-	public static void solveA() {
+	public static int solveA(String datastream) {
+		return findCharsBeforeObject(datastream, 4);
 	}
 
-	public static void solveB() {
+	public static int solveB(String datastream) {
+		return findCharsBeforeObject(datastream, 14);
+	}
+
+	public static int findCharsBeforeObject(String datastream, int objectSize) {
+		for (int i = objectSize; i < datastream.length(); i++) {
+			String lastFour = datastream.substring(i - objectSize, i);
+			if (lastFour.chars().distinct().count() == objectSize)
+				return i;
+		}
+		return -1;
 	}
 }
