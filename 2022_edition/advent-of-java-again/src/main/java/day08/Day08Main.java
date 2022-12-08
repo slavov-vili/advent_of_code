b43faecd6c82ec4f52fd4db1a4a63f465e01eaa3
Velislav Slavov <velislav.slavov@cas.de>
2022-12-08 19:59:35 +0100

    AOC 2022: Add solution to day08

--- 2022_edition/advent-of-java-again/src/main/java/day08/Day08Main.java
+++ 2022_edition/advent-of-java-again/src/main/java/day08/Day08Main.java
@@ -1,18 +1,133 @@
 package day08;
 
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
 
 import utils.AdventOfCodeUtils;
+import utils.Grid2D;
 
 public class Day08Main {
 
 	public static void main(String[] args) {
+		Grid2D<Integer> forest = parseInput();
+
+		System.out.println("Trees visible from the outside: " + solveA(forest));
+
+		System.out.println("Max scenic score: " + solveB(forest));
+	}
+
+	public static int solveA(Grid2D<Integer> forest) {
+		var visiblePositions = new HashSet<Point>();
+		visiblePositions.addAll(findVisibleFromTop(forest));
+		visiblePositions.addAll(findVisibleFromLeft(forest));
+		visiblePositions.addAll(findVisibleFromRight(forest));
+		visiblePositions.addAll(findVisibleFromBottom(forest));
+		return visiblePositions.size();
+	}
+
+	public static int solveB(Grid2D<Integer> forest) {
+		var iterator = forest.positionIterator();
+		var maxScore = Integer.MIN_VALUE;
+		while (iterator.hasNext()) {
+			var curPosition = iterator.next();
+			var curScore = calcScenicScore(curPosition, forest);
+
+			if (curScore > maxScore)
+				maxScore = curScore;
+		}
+		return maxScore;
+	}
+
+	public static Set<Point> findVisibleFromTop(Grid2D<Integer> forest) {
+		return findVisibleFromOutside(new Point(0, 0), forest, Grid2D.RIGHT, Grid2D.DOWN);
+	}
+
+	public static Set<Point> findVisibleFromLeft(Grid2D<Integer> forest) {
+		return findVisibleFromOutside(new Point(0, 0), forest, Grid2D.DOWN, Grid2D.RIGHT);
+	}
+
+	public static Set<Point> findVisibleFromRight(Grid2D<Integer> forest) {
+		return findVisibleFromOutside(new Point(forest.getRowCount() - 1, forest.getColumnCount() - 1), forest,
+				Grid2D.UP, Grid2D.LEFT);
+	}
+
+	public static Set<Point> findVisibleFromBottom(Grid2D<Integer> forest) {
+		return findVisibleFromOutside(new Point(forest.getRowCount() - 1, forest.getColumnCount() - 1), forest,
+				Grid2D.LEFT, Grid2D.UP);
+	}
+
+	public static Set<Point> findVisibleFromOutside(Point startPosition, Grid2D<Integer> forest,
+			UnaryOperator<Point> movingDirection, UnaryOperator<Point> lookingDirection) {
+		var visiblePositions = new HashSet<Point>();
+		var curPosition = startPosition;
+		while (forest.hasPosition(curPosition)) {
+			visiblePositions.addAll(findVisibleInLine(curPosition, forest, lookingDirection));
+
+			curPosition = movingDirection.apply(curPosition);
+		}
+		return visiblePositions;
+	}
+
+	public static List<Point> findVisibleInLine(Point startPosition, Grid2D<Integer> forest,
+			UnaryOperator<Point> lookingDirection) {
+		var visiblePositions = new ArrayList<Point>();
+		var curPosition = startPosition;
+		var curMaxHeight = Integer.MIN_VALUE;
+		while (forest.hasPosition(curPosition)) {
+			var curHeight = forest.get(curPosition);
+			if (curHeight > curMaxHeight) {
+				visiblePositions.add(curPosition);
+				curMaxHeight = curHeight;
+			}
+
+			curPosition = lookingDirection.apply(curPosition);
+		}
+
+		return visiblePositions;
+	}
+
+	public static int calcScenicScore(Point position, Grid2D<Integer> forest) {
+		return Grid2D.DIRECTIONS.stream().mapToInt(dir -> countVisibleTreesInDirection(position, forest, dir))
+				.reduce((x, y) -> x * y).getAsInt();
+	}
+
+	public static int countVisibleTreesInDirection(Point position, Grid2D<Integer> forest,
+			UnaryOperator<Point> direction) {
+		var maxHeight = forest.get(position);
+		var nextPosition = direction.apply(position);
+		int treeCount = 0;
+		while (forest.hasPosition(nextPosition)) {
+			var nextHeight = forest.get(nextPosition);
+			if (nextHeight < maxHeight)
+				treeCount++;
+			else if (nextHeight >= maxHeight) {
+				treeCount++;
+				break;
+			}
+
+			nextPosition = direction.apply(nextPosition);
+		}
+		return treeCount;
+	}
+
+	public static Grid2D<Integer> parseInput() {
 		List<String> input = AdventOfCodeUtils.readInput(Day08Main.class);
-	}
+		int rowCount = input.size();
+		int columnCount = input.get(0).length();
+		var grid = new Grid2D<Integer>(columnCount, rowCount);
 
-	public static void solveA() {
-	}
+		for (int row = 0; row < rowCount; row++) {
+			var trees = Arrays.stream(input.get(row).split("")).map(Integer::parseInt).collect(Collectors.toList());
 
-	public static void solveB() {
+			for (int col = 0; col < columnCount; col++)
+				grid.set(new Point(row, col), trees.get(col));
+		}
+		return grid;
 	}
 }
--- Advent_of_Code_Utils/aoc-utils/src/main/java/utils/Grid2D.java
+++ Advent_of_Code_Utils/aoc-utils/src/main/java/utils/Grid2D.java
@@ -1,49 +1,101 @@
 package utils;
 
 import java.awt.Point;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
 
+// Point.x = row
+// Point.y = column
 public class Grid2D<T> {
+
+	public static final UnaryOperator<Point> UP = point -> new Point(point.x - 1, point.y);
+	public static final UnaryOperator<Point> RIGHT = point -> new Point(point.x, point.y + 1);
+	public static final UnaryOperator<Point> DOWN = point -> new Point(point.x + 1, point.y);
+	public static final UnaryOperator<Point> LEFT = point -> new Point(point.x, point.y - 1);
+	public static final List<UnaryOperator<Point>> DIRECTIONS = List.of(UP, RIGHT, DOWN, LEFT);
+
 	private Object[][] grid;
 
-	public Grid2D(Point dimensions) {
-		this.grid = new Object[dimensions.y][dimensions.x];
+	public Grid2D(int rowCount, int columnCount) {
+		this.grid = new Object[rowCount][columnCount];
 	}
-	
-	@SuppressWarnings("unchecked")
+
 	public T get(Point position) {
 		checkPosition(position);
-		return (T) this.grid[position.y][position.x];
+		return (T) this.grid[position.x][position.y];
 	}
-	
+
 	public T set(Point position, T newValue) {
 		checkPosition(position);
 		T oldValue = this.get(position);
-		this.grid[position.y][position.x] = newValue;
+		this.grid[position.x][position.y] = newValue;
 		return oldValue;
 	}
-	
-	public int getWidth() {
+
+	public int getColumnCount() {
 		return this.grid.length;
 	}
-	
-	public int getHeight() {
+
+	public int getRowCount() {
 		return this.grid[0].length;
 	}
-	
+
 	public int getSize() {
-		return this.getWidth() * this.getHeight();
+		return this.getColumnCount() * this.getRowCount();
 	}
-	
+
+	public boolean isLastPosition(Point positionToCheck) {
+		return positionToCheck.equals(new Point(this.getRowCount() - 1, this.getColumnCount() - 1));
+	}
+
+	public boolean hasPosition(Point positionToCheck) {
+		return (positionToCheck.x >= 0) && (positionToCheck.x < this.getRowCount()) && (positionToCheck.y >= 0)
+				&& (positionToCheck.y < this.getColumnCount());
+	}
+
 	private void checkPosition(Point positionToCheck) {
-		if ((positionToCheck.x < 0) || (positionToCheck.x >= this.getWidth())) {
-			throw new IndexOutOfBoundsException("Width: " + positionToCheck.x);
-		} else if ((positionToCheck.y < 0) || (positionToCheck.y >= this.getHeight())) {
-			throw new IndexOutOfBoundsException("Height: " + positionToCheck.y);
+		if (!hasPosition(positionToCheck)) {
+			throw new IndexOutOfBoundsException("Position not in grid: " + positionToCheck);
 		}
 	}
-	
+
+	public Iterator<Point> positionIterator() {
+		return new PositionIterator(this.getRowCount(), this.getColumnCount(), this::isLastPosition);
+	}
+
 	@Override
 	public String toString() {
 		return this.grid.toString();
+	}
+
+	public static class PositionIterator implements Iterator<Point> {
+		public static final Point INITIAL_POSITION = new Point(0, -1);
+
+		public int rowCount, columnCount;
+		public Predicate<Point> isLastPosition;
+		public Point curPosition;
+
+		public PositionIterator(int rowCount, int columnCount, Predicate<Point> isLastPosition) {
+			this.rowCount = rowCount;
+			this.columnCount = columnCount;
+			this.isLastPosition = isLastPosition;
+			this.curPosition = INITIAL_POSITION;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return INITIAL_POSITION.equals(this.curPosition) || !isLastPosition.test(this.curPosition);
+		}
+
+		@Override
+		public Point next() {
+			int newColumn = this.curPosition.y + 1;
+			int newRow = (newColumn == this.columnCount) ? this.curPosition.x + 1 : this.curPosition.x;
+			this.curPosition = new Point(newRow, newColumn % this.columnCount);
+			return this.curPosition;
+		}
+
 	}
 }
