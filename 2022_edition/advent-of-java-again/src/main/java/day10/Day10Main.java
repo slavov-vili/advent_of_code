25ec91d5953b111d2977b5e6b0a49de95eb11aaf
Velislav Slavov <velislav.slavov@cas.de>
2022-12-10 14:57:56 +0100

    AOC 2022: Add solution to day10, part2

--- 2022_edition/advent-of-java-again/src/main/java/day10/Day10Main.java
+++ 2022_edition/advent-of-java-again/src/main/java/day10/Day10Main.java
@@ -2,6 +2,7 @@ package day10;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import utils.AdventOfCodeUtils;
 
@@ -11,18 +12,35 @@ public class Day10Main {
 	public static final int LAST_CYCLE = 220;
 	public static final List<Integer> relevantCycles = List.of(20, 60, 100, 140, 180, LAST_CYCLE);
 
-	public static void main(String[] args) {
-		List<String> input = AdventOfCodeUtils.readInput(Day10Main.class);
+	public static final int CRT_WIDTH = 40;
+	public static final int CRT_HEIGHT = 6;
+	public static final int CRT_SIZE = CRT_WIDTH * CRT_HEIGHT;
+	public static final String PIXEL_LIT = "#";
+	public static final String PIXEL_DARK = " ";
 
-		System.out.println("Sum of relevant signal strengths: " + solveA(input));
+	public static void main(String[] args) {
+		var cycleValues = getCycleValues(AdventOfCodeUtils.readInput(Day10Main.class), LAST_CYCLE);
+
+		System.out.println("Sum of relevant signal strengths: " + solveA(cycleValues));
+
+		System.out.println("CRT Screen:\n" + solveB(cycleValues));
 	}
 
-	public static int solveA(List<String> program) {
-		var cycleValues = getCycleValues(program, LAST_CYCLE);
+	public static int solveA(List<Integer> cycleValues) {
 		return relevantCycles.stream().mapToInt(cycle -> cycle * cycleValues.get(cycle - 1)).sum();
 	}
 
-	public static void solveB() {
+	public static String solveB(List<Integer> cycleValues) {
+		return IntStream.range(0, CRT_SIZE).mapToObj(i -> evalCRTCycle(i % 40, cycleValues.get(i)))
+				.reduce(String::concat).get();
+	}
+
+	public static String evalCRTCycle(int curPixelIndex, int registerValue) {
+		int pixelDist = Math.abs(curPixelIndex - registerValue);
+		String crtCycleValue = (pixelDist <= 1) ? PIXEL_LIT : PIXEL_DARK;
+		if (curPixelIndex == CRT_WIDTH - 1)
+			crtCycleValue += "\n";
+		return crtCycleValue;
 	}
 
 	public static List<Integer> getCycleValues(List<String> program, int maxCycle) {
