5de5c6296b2c67d4b9eb6c71bdeb8c76239039d2
vilskin <slavov.vili@gmail.com>
2023-07-31 15:53:07 +0200

    Add Solution to day16, part2

--- 2022_edition/advent-of-java-again/src/main/java/day16/Day16Main.java
+++ 2022_edition/advent-of-java-again/src/main/java/day16/Day16Main.java
@@ -1,18 +1,11 @@
 package day16;
 
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-
 import utils.AdventOfCodeUtils;
 import utils.DijkstraShortestPathFinder;
+
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Day16Main {
 	public static final Pattern VALVE_PATTERN = Pattern
@@ -20,25 +13,33 @@ public class Day16Main {
 	public static final int MINUTES = 30;
 	public static final int OPEN_TIME = 1;
 	public static final String START_VALVE = "AA";
+
 	public static final int ELEPHANT_TRAINING_TIME = 4;
+	public static final int MINUTES_WITH_ELEPHANT = MINUTES - ELEPHANT_TRAINING_TIME;
 
 	public static void main(String[] args) {
 		var valveDistanceMap = findValveShortestPaths(parseInput());
-		var valvesToOpen = valveDistanceMap.keySet().stream().filter(v -> valveDistanceMap.get(v).rate() > 0)
-				.collect(Collectors.toSet());
 
-		System.out.println("Max pressure = " + solveA(valveDistanceMap, valvesToOpen));
+		System.out.println("Max pressure = " + solveA(valveDistanceMap));
 
-		System.out.println("Max pressure with elephant = " + solveB(valveDistanceMap, valvesToOpen));
+		System.out.println("Max pressure with elephant = " + solveB(valveDistanceMap));
 	}
 
-	public static int solveA(Map<String, ValveWithDistances> valveDistanceMap, Set<String> valvesToOpen) {
+	public static int solveA(Map<String, ValveWithDistances> valveDistanceMap) {
+		var valvesToOpen = valveDistanceMap.keySet().stream().filter(v -> valveDistanceMap.get(v).rate() > 0 ).collect(Collectors.toSet());
 		return findMaxPotentialPressure(START_VALVE, 0, valvesToOpen, valveDistanceMap);
 	}
 
-	public static int solveB(Map<String, ValveWithDistances> valveDistanceMap, Set<String> valvesToOpen) {
-		return findMaxPotentialPressure(START_VALVE, 0, START_VALVE, 0, 0, valvesToOpen, new HashMap<>(),
-				valveDistanceMap);
+	public static int solveB(Map<String, ValveWithDistances> valveDistanceMap) {
+		var cleanValveDistanceMap = new HashMap<>(valveDistanceMap);
+		valveDistanceMap.keySet().stream().filter(v -> valveDistanceMap.get(v).rate() == 0 && !START_VALVE.equals(v)).forEach(cleanValveDistanceMap::remove);
+		var pathPressures = mapPathsToPressure(List.of(START_VALVE), 0, 0, cleanValveDistanceMap);
+		pathPressures.remove(List.of(START_VALVE));
+		var cleanPathPressures = pathPressures.keySet().stream()
+				.filter(p -> p.size() >= 7) // otherwise it will never finish...
+				.collect(Collectors.toMap(p -> p.subList(1, p.size()), pathPressures::get));
+
+		return findMaxIndependentPairs(cleanPathPressures);
 	}
 
 	public static int findMaxPotentialPressure(String curValve, int curMinute, Set<String> valvesToOpen,
@@ -50,7 +51,7 @@ public class Day16Main {
 		for (String nextValve : valvesToOpen) {
 			int dist = valveDistanceMap.get(curValve).getDistanceTo(nextValve);
 			int openMinute = curMinute + dist + OPEN_TIME;
-			if (openMinute > MINUTES)
+			if (openMinute >= MINUTES)
 				continue;
 			int curPotentialPressure = (MINUTES - openMinute) * valveDistanceMap.get(nextValve).rate();
 			var newValvesToOpen = new HashSet<>(valvesToOpen);
@@ -64,108 +65,51 @@ public class Day16Main {
 		return maxPotentialPressure;
 	}
 
-	// FIXME: wtf is happening here, this is SO complicated!
-	public static int findMaxPotentialPressure(String myValve, int myFreeMinute, String elephantValve,
-			int elephantFreeMinute, int curMinute, Set<String> valvesToOpen, Map<Integer, Integer> stateToMaxPressure,
-			Map<String, ValveWithDistances> valveDistanceMap) {
-		int maxPotentialPressure = 0;
-		if (curMinute > (MINUTES - ELEPHANT_TRAINING_TIME) || valvesToOpen.isEmpty())
-			return maxPotentialPressure;
+	public static Map<List<String>, Integer> mapPathsToPressure(List<String> curPath, int curPressure, int curMinute,
+																Map<String, ValveWithDistances> valveDistanceMap) {
+		var nextValves = new HashSet<>(valveDistanceMap.keySet());
+		curPath.forEach(nextValves::remove);
 
-		var curState = getState(myValve, myFreeMinute, elephantValve, elephantFreeMinute, curMinute, valvesToOpen);
-		var curReverseState = getState(elephantValve, elephantFreeMinute, myValve, myFreeMinute, curMinute,
-				valvesToOpen);
-		if (stateToMaxPressure.containsKey(curState))
-			return stateToMaxPressure.get(curState);
-		else if (stateToMaxPressure.containsKey(curReverseState))
-			return stateToMaxPressure.get(curReverseState);
-		System.out.printf("Minute %d, myValve = %s, elephantValve = %s\n", curMinute, myValve, elephantValve);
+		var openMinutesMap = nextValves.stream().collect(Collectors.toMap(v -> v, v ->
+				curMinute + valveDistanceMap.get(curPath.get(curPath.size()-1)).getDistanceTo(v) + OPEN_TIME));
+		openMinutesMap.keySet().stream().filter(v -> openMinutesMap.get(v) >= MINUTES_WITH_ELEPHANT).forEach(nextValves::remove);
 
-		if (myFreeMinute <= curMinute) {
-			System.out.println("I'm free!");
-			for (String myNextValve : valvesToOpen) {
-				int curPotentialPressure = 0;
-				int myOpenMinute = curMinute + valveDistanceMap.get(myValve).getDistanceTo(myNextValve) + OPEN_TIME;
+		if (nextValves.isEmpty())
+			return Map.of(curPath, curPressure);
 
-				if (myOpenMinute > MINUTES - ELEPHANT_TRAINING_TIME)
-					continue;
-
-				curPotentialPressure = (MINUTES - ELEPHANT_TRAINING_TIME - myOpenMinute)
-						* valveDistanceMap.get(myNextValve).rate();
-				var remainingValvesToOpen = new HashSet<>(valvesToOpen);
-				remainingValvesToOpen.remove(myNextValve);
-//				System.out.printf("My next valve %s is %d step(s) away => myPotentialPressure = %d\n", myNextValve,
-//						valveDistanceMap.get(myValve).getDistanceTo(myNextValve), curPotentialPressure);
-				int state = getState(myNextValve, myOpenMinute, elephantValve, elephantFreeMinute, curMinute,
-						remainingValvesToOpen);
-				int reverseState = getState(elephantValve, elephantFreeMinute, myNextValve, myOpenMinute, curMinute,
-						remainingValvesToOpen);
-				if (stateToMaxPressure.containsKey(state))
-					return stateToMaxPressure.get(state);
-				else if (stateToMaxPressure.containsKey(reverseState))
-					return stateToMaxPressure.get(reverseState);
-
-				int elephantMaxPressure = elephantForLoop(myNextValve, myOpenMinute, elephantValve, elephantFreeMinute,
-						curMinute, remainingValvesToOpen, stateToMaxPressure, valveDistanceMap);
-				stateToMaxPressure.put(state, elephantMaxPressure);
-				stateToMaxPressure.put(reverseState, elephantMaxPressure);
-				curPotentialPressure += elephantMaxPressure;
-
-				if (curPotentialPressure > maxPotentialPressure)
-					maxPotentialPressure = curPotentialPressure;
+		var pathsToPressure = new HashMap<List<String>, Integer>();
+		pathsToPressure.put(curPath, curPressure);
+		for (String nextValve : nextValves) {
+			int openMinute = openMinutesMap.get(nextValve);
+			int nextPressure = curPressure + (MINUTES_WITH_ELEPHANT - openMinute) * valveDistanceMap.get(nextValve).rate();
+			var nextPath = new ArrayList<>(curPath);
+			nextPath.add(nextValve);
+			var newPathsToPressure = mapPathsToPressure(nextPath, nextPressure, openMinute, valveDistanceMap);
+			for (var path : newPathsToPressure.keySet()) {
+				int prevPressure = pathsToPressure.getOrDefault(path, 0);
+				pathsToPressure.put(path, Integer.max(prevPressure, newPathsToPressure.get(path)));
 			}
-		} else if (elephantFreeMinute == curMinute) {
-			maxPotentialPressure = elephantForLoop(myValve, myFreeMinute, elephantValve, elephantFreeMinute, curMinute,
-					valvesToOpen, stateToMaxPressure, valveDistanceMap);
-		} else {
-			maxPotentialPressure = findMaxPotentialPressure(myValve, myFreeMinute, elephantValve, elephantFreeMinute,
-					curMinute + 1, valvesToOpen, stateToMaxPressure, valveDistanceMap);
-			curState = getState(myValve, myFreeMinute, elephantValve, elephantFreeMinute, curMinute + 1, valvesToOpen);
-			curReverseState = getState(elephantValve, elephantFreeMinute, myValve, myFreeMinute, curMinute + 1,
-					valvesToOpen);
 		}
 
-		stateToMaxPressure.put(curState, maxPotentialPressure);
-		stateToMaxPressure.put(curReverseState, maxPotentialPressure);
-
-		return maxPotentialPressure;
+		return pathsToPressure;
 	}
 
-	public static int elephantForLoop(String myNextValve, int myOpenMinute, String elephantValve,
-			int elephantFreeMinute, int curMinute, Set<String> valvesToOpen, Map<Integer, Integer> stateToMaxPressure,
-			Map<String, ValveWithDistances> valveDistanceMap) {
-		int maxPotentialPressure = 0;
-		if (elephantFreeMinute <= curMinute) {
-			System.out.println("Elephant is free!");
-			for (String elephantNextValve : valvesToOpen) {
-				int elephantOpenMinute = curMinute
-						+ valveDistanceMap.get(elephantValve).getDistanceTo(elephantNextValve) + OPEN_TIME;
-
-				if (elephantOpenMinute > MINUTES - ELEPHANT_TRAINING_TIME)
-					continue;
-
-				int curPotentialPressure = (MINUTES - ELEPHANT_TRAINING_TIME - elephantOpenMinute)
-						* valveDistanceMap.get(elephantNextValve).rate();
-				var remainingValvesToOpen = new HashSet<>(valvesToOpen);
-				remainingValvesToOpen.remove(elephantNextValve);
-
-				curPotentialPressure += findMaxPotentialPressure(myNextValve, myOpenMinute, elephantNextValve,
-						elephantOpenMinute, curMinute + 1, remainingValvesToOpen, stateToMaxPressure, valveDistanceMap);
-
-				if (curPotentialPressure > maxPotentialPressure) {
-					maxPotentialPressure = curPotentialPressure;
-				}
-			}
-		} else {
-			maxPotentialPressure = findMaxPotentialPressure(myNextValve, myOpenMinute, elephantValve,
-					elephantFreeMinute, curMinute + 1, valvesToOpen, stateToMaxPressure, valveDistanceMap);
+	public static int findMaxIndependentPairs(Map<List<String>, Integer> pathPressures) {
+		int count = 0;
+		// TODO: try using indices instead, maybe takes less heap space?
+		var pairPathPressures = new HashMap<Set<List<String>>, Integer>();
+		for (var path : pathPressures.keySet()) {
+			count++;
+			System.out.printf("Finding pairs %d / %d\n", count, pathPressures.size());
+			var nextPaths = pathPressures.keySet().stream()
+					.filter(p -> p.stream().noneMatch(path::contains)) // are independent
+					.filter(p -> !pairPathPressures.containsKey(Set.of(path, p))).toList(); // pair hasn't been seen
+			var newPairPathPressures = nextPaths.stream()
+					.collect(Collectors.toMap(p -> Set.of(path, p), p -> pathPressures.get(path) + pathPressures.get(p)));
+			pairPathPressures.putAll(newPairPathPressures);
 		}
-		return maxPotentialPressure;
-	}
 
-	public static int getState(String valve1, int freeMinute1, String valve2, int freeMinute2, int curMinute,
-			Set<String> valvesToOpen) {
-		return Objects.hash(valve1, freeMinute1, valve2, freeMinute2, curMinute, valvesToOpen);
+		return pairPathPressures.values().stream().max(Integer::compare).orElseGet(() -> 0);
 	}
 
 	public static Map<String, ValveWithDistances> findValveShortestPaths(Map<String, Valve> valveMap) {
@@ -177,6 +121,11 @@ public class Day16Main {
 					new ValveWithDistances(valve, valveMap.get(valve).rate(), shortestPathFinder.findAll(valve)));
 		}
 		return valveDistanceMap;
+	}
+
+	public static Map<String, Valve> parseInput() {
+		return AdventOfCodeUtils.readInput(Day16Main.class).stream().map(Valve::fromLine)
+				.collect(Collectors.toMap(Valve::name, v -> v));
 	}
 
 	public record Valve(String name, int rate, List<String> neighbors) {
@@ -192,10 +141,5 @@ public class Day16Main {
 		public int getDistanceTo(String otherValve) {
 			return this.neighborDistances.getOrDefault(otherValve, Integer.MAX_VALUE);
 		}
-	}
-
-	public static Map<String, Valve> parseInput() {
-		return AdventOfCodeUtils.readInput(Day16Main.class).stream().map(Valve::fromLine)
-				.collect(Collectors.toMap(v -> v.name(), v -> v));
 	}
 }
