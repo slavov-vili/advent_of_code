8cb771dbdee771d79e3a77f986c880a790bdf40b
vilskin <slavov.vili@gmail.com>
2024-04-05 13:50:57 +0200

    AOC23: Add solution attempt for day10

--- 2023_edition/aoc23/src/main/java/day10/Day10Main.java
+++ 2023_edition/aoc23/src/main/java/day10/Day10Main.java
@@ -2,8 +2,8 @@ package day10;
 
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -28,32 +28,71 @@ public class Day10Main {
 	public static void main(String[] args) {
 		var grid = parseGrid();
 		Point startPos = grid.getPositionsWhere(ch -> ch.isStart()).get(0);
-		var mainLoop = findMainLoop(startPos, grid);
+		var mainLoop = findMainLoop(grid.get(startPos), grid);
+		printLoop(mainLoop, grid);
 
 		System.out.println("Most steps from start: " + solveA(mainLoop));
+
+		System.out.println("Enclosed tiles: " + solveB(mainLoop));
 	}
 
-	public static int solveA(List<Point> mainLoop) {
+	public static long solveA(List<Pipe> mainLoop) {
 		var pathFinder = new DijkstraShortestPathFinder<Integer>(i -> ListUtils.getNeighborsOf(mainLoop, i));
 		pathFinder.findAll(0);
-		return IntStream.range(0, mainLoop.size()).map(pathFinder::getDistFromStart).max().getAsInt();
+		return IntStream.range(0, mainLoop.size()).mapToLong(pathFinder::getDistFromStart).max().getAsLong();
 	}
 
-	public static void solveB() {
+	public static int solveB(List<Pipe> mainLoop) {
+		return findEnclosedTiles(mainLoop).size();
 	}
 
-	public static List<Point> findMainLoop(Point startPos, Grid2D<Pipe> grid) {
-		List<Point> mainLoop = new ArrayList<>();
-		mainLoop.add(startPos);
-		Point prevPos = startPos;
-		Point curPos = grid.get(startPos).findValidConnections(grid).iterator().next();
-		while (!curPos.equals(startPos)) {
-			mainLoop.add(curPos);
-			Point nextPos = grid.get(prevPos).followPipe(grid.get(curPos)).get(0);
-			prevPos = curPos;
-			curPos = nextPos;
+	public static List<Pipe> findMainLoop(Pipe startPipe, Grid2D<Pipe> grid) {
+		List<Pipe> mainLoop = new ArrayList<>();
+		mainLoop.add(startPipe);
+		Pipe prevPipe = startPipe;
+		Pipe curPipe = startPipe.findValidConnections(grid).iterator().next();
+		while (!curPipe.equals(startPipe)) {
+			mainLoop.add(prevPipe);
+			Point nextPos = prevPipe.followPipe(curPipe).get(0);
+			prevPipe = curPipe;
+			curPipe = grid.get(nextPos);
 		}
 		return mainLoop;
+	}
+
+	// FIXME: find something else (e.x. all surrounding must be part of path or also
+	// inside)
+	public static Set<Point> findEnclosedTiles(List<Pipe> mainLoop) {
+		Set<Point> encosedTiles = new HashSet<>();
+
+		var loopPositions = mainLoop.stream().map(Pipe::position).collect(Collectors.toSet());
+		int minRow = loopPositions.stream().mapToInt(pos -> pos.x).min().getAsInt();
+		int maxRow = loopPositions.stream().mapToInt(pos -> pos.x).max().getAsInt();
+		int minCol = loopPositions.stream().mapToInt(pos -> pos.y).min().getAsInt();
+		int maxCol = loopPositions.stream().mapToInt(pos -> pos.y).max().getAsInt();
+		int row = minRow;
+		while (row <= maxRow) {
+			int col = minCol;
+			int pathGroups = 0;
+			boolean groupStarted = false;
+			while (col <= maxCol) {
+				Point curPos = new Point(row, col);
+				if (loopPositions.contains(curPos)) {
+					if (!groupStarted) {
+						groupStarted = true;
+						pathGroups++;
+					}
+				} else {
+					if (groupStarted)
+						groupStarted = false;
+					if (pathGroups % 2 == 1)
+						encosedTiles.add(curPos);
+				}
+				col++;
+			}
+			row++;
+		}
+		return encosedTiles;
 	}
 
 	record Pipe(char symbol, Point position) {
@@ -61,20 +100,21 @@ public class Day10Main {
 			if (!(this.connectsTo(nextPipe)))
 				return List.of();
 
-			return nextPipe.findConnections().stream().filter(pos -> !pos.equals(this.position)).toList();
+			return nextPipe.findConnectionPositions().stream().filter(pos -> !pos.equals(this.position)).toList();
 		}
 
 		public boolean connectsTo(Pipe otherPipe) {
-			return this.findConnections().contains(otherPipe.position)
-					&& otherPipe.findConnections().contains(this.position);
+			return this.findConnectionPositions().contains(otherPipe.position)
+					&& otherPipe.findConnectionPositions().contains(this.position);
 		}
 
-		public Set<Point> findValidConnections(Grid2D<Pipe> grid) {
-			return this.findConnections().stream()
-					.filter(pos -> grid.hasPosition(pos) && this.connectsTo(grid.get(pos))).collect(Collectors.toSet());
+		public Set<Pipe> findValidConnections(Grid2D<Pipe> grid) {
+			return this.findConnectionPositions().stream()
+					.filter(pos -> grid.hasPosition(pos) && this.connectsTo(grid.get(pos))).map(grid::get)
+					.collect(Collectors.toSet());
 		}
 
-		public Set<Point> findConnections() {
+		public Set<Point> findConnectionPositions() {
 			return switch (this.symbol) {
 			case VERTICAL -> Set.of(PointUtils.UP.apply(this.position), PointUtils.DOWN.apply(this.position));
 			case HORIZONTAL -> Set.of(PointUtils.LEFT.apply(this.position), PointUtils.RIGHT.apply(this.position));
@@ -109,5 +149,20 @@ public class Day10Main {
 			}
 		}
 		return grid;
+	}
+
+	public static void printLoop(List<Pipe> loop, Grid2D<Pipe> grid) {
+		int minX = loop.stream().mapToInt(pipe -> pipe.position.x).min().getAsInt();
+		int maxX = loop.stream().mapToInt(pipe -> pipe.position.x).max().getAsInt();
+		int minY = loop.stream().mapToInt(pipe -> pipe.position.y).min().getAsInt();
+		int maxY = loop.stream().mapToInt(pipe -> pipe.position.y).max().getAsInt();
+		for (int row = minX; row <= maxX; row++) {
+			for (int col = minY; col <= maxY; col++) {
+				Point pos = new Point(row, col);
+				char symbol = grid.hasPosition(pos) ? grid.get(pos).symbol() : GROUND;
+				System.out.print(symbol);
+			}
+			System.out.println();
+		}
 	}
 }
