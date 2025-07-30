0ef1b7e51f8c19a87f2aebccd8eb7481fab8c0cc
vilskin <slavov.vili@gmail.com>
2025-07-30 14:30:59 +0200

    AOC 2021: Reformat day 22

--- 2021_edition/advent-of-java-2-advent-harder/src/main/java/day22/Day22Main.java
+++ 2021_edition/advent-of-java-2-advent-harder/src/main/java/day22/Day22Main.java
@@ -17,103 +17,103 @@ import utils.ThreeDPoint;
 
 public class Day22Main {
 
-	public static final Pattern LINE_PATTERN = Pattern
-			.compile("(on|off) x=(-?\\d+)..(-?\\d+),y=(-?\\d+)..(-?\\d+),z=(-?\\d+)..(-?\\d+)");
+    public static final Pattern LINE_PATTERN = Pattern
+            .compile("(on|off) x=(-?\\d+)..(-?\\d+),y=(-?\\d+)..(-?\\d+),z=(-?\\d+)..(-?\\d+)");
 
-	public static void main(String[] args) {
-		List<RebootInstruction> instructions = parseInput();
+    public static void main(String[] args) {
+        List<RebootInstruction> instructions = parseInput();
 
-		System.out.println("On cubes in initialization area: " + solveA(instructions));
+        System.out.println("On cubes in initialization area: " + solveA(instructions));
 
-		System.out.println("On cubes total: " + solveB(instructions));
-	}
+        System.out.println("On cubes total: " + solveB(instructions));
+    }
 
-	public static int solveA(List<RebootInstruction> instructions) {
-		Set<ThreeDPoint> onCubes = new HashSet<>();
-		for (RebootInstruction instruction : instructions) {
-			if (isWithinInitArea(instruction)) {
-				Set<ThreeDPoint> cubes = getCubes(instruction);
-				if (instruction.isOn())
-					onCubes.addAll(cubes);
-				else
-					onCubes.removeAll(cubes);
-			}
-		}
-		return onCubes.size();
-	}
+    public static int solveA(List<RebootInstruction> instructions) {
+        Set<ThreeDPoint> onCubes = new HashSet<>();
+        for (RebootInstruction instruction : instructions) {
+            if (isWithinInitArea(instruction.getArea())) {
+                Set<ThreeDPoint> cubes = getCubes(instruction.getArea());
+                if (instruction.isOn())
+                    onCubes.addAll(cubes);
+                else
+                    onCubes.removeAll(cubes);
+            }
+        }
+        return onCubes.size();
+    }
 
-	public static long solveB(List<RebootInstruction> instructions) {
-		Collection<Cuboid> noOverlapsOnCuboids = new ArrayList<>();
-		for (RebootInstruction newInstruction : instructions) {
-			if (newInstruction.isOn()) {
-				Collection<Cuboid> additions = Arrays.asList(newInstruction.getArea());
-				for (Cuboid onCuboid : noOverlapsOnCuboids) {
-					Collection<Cuboid> newAdditions = new ArrayList<>();
-					for (Cuboid addition : additions)
-						newAdditions.addAll(onCuboid.calcAdditionsFor(addition));
+    public static long solveB(List<RebootInstruction> instructions) {
+        Collection<Cuboid> noOverlapsOnCuboids = new ArrayList<>();
+        for (RebootInstruction newInstruction : instructions) {
+            if (newInstruction.isOn()) {
+                Collection<Cuboid> additions = Arrays.asList(newInstruction.getArea());
+                for (Cuboid onCuboid : noOverlapsOnCuboids) {
+                    Collection<Cuboid> newAdditions = new ArrayList<>();
+                    for (Cuboid addition : additions)
+                        newAdditions.addAll(onCuboid.calcAdditionsFor(addition));
 
-					additions = newAdditions;
-				}
+                    additions = newAdditions;
+                }
 
-				noOverlapsOnCuboids.addAll(additions);
-			} else {
-				Collection<Cuboid> newOnCuboids = new ArrayList<>();
-				for (Cuboid onCuboid : noOverlapsOnCuboids) {
-					Optional<Cuboid> intersection = onCuboid.findIntersectionWith(newInstruction.getArea());
-					if (intersection.isPresent())
-						newOnCuboids.addAll(onCuboid.split(intersection.get()));
-					else
-						newOnCuboids.add(onCuboid);
-				}
+                noOverlapsOnCuboids.addAll(additions);
+            } else {
+                Collection<Cuboid> newOnCuboids = new ArrayList<>();
+                for (Cuboid onCuboid : noOverlapsOnCuboids) {
+                    Optional<Cuboid> intersection = onCuboid.findIntersectionWith(newInstruction.getArea());
+                    if (intersection.isPresent())
+                        newOnCuboids.addAll(onCuboid.split(intersection.get()));
+                    else
+                        newOnCuboids.add(onCuboid);
+                }
 
-				noOverlapsOnCuboids = newOnCuboids;
-			}
-		}
+                noOverlapsOnCuboids = newOnCuboids;
+            }
+        }
 
-		return noOverlapsOnCuboids.stream().mapToLong(Cuboid::calcSize).sum();
-	}
+        return noOverlapsOnCuboids.stream().mapToLong(Cuboid::calcSize).sum();
+    }
 
-	public static boolean isWithinInitArea(RebootInstruction instruction) {
-		boolean areXSmaller = instruction.getFromX() < -50 && instruction.getToX() < -50;
-		boolean areXBigger = instruction.getFromX() > 50 && instruction.getToX() > 50;
-		boolean isXOutside = areXSmaller || areXBigger;
+    public static boolean isWithinInitArea(Cuboid area) {
+        boolean areXSmaller = area.getFromX() < -50 && area.getToX() < -50;
+        boolean areXBigger = area.getFromX() > 50 && area.getToX() > 50;
+        boolean isXOutside = areXSmaller || areXBigger;
 
-		boolean areYSmaller = instruction.getFromY() < -50 && instruction.getToY() < -50;
-		boolean areYBigger = instruction.getFromY() > 50 && instruction.getToY() > 50;
-		boolean isYOutside = areYSmaller || areYBigger;
+        boolean areYSmaller = area.getFromY() < -50 && area.getToY() < -50;
+        boolean areYBigger = area.getFromY() > 50 && area.getToY() > 50;
+        boolean isYOutside = areYSmaller || areYBigger;
 
-		boolean areZSmaller = instruction.getFromZ() < -50 && instruction.getToZ() < -50;
-		boolean areZBigger = instruction.getFromZ() > 50 && instruction.getToZ() > 50;
-		boolean isZOutside = areZSmaller || areZBigger;
+        boolean areZSmaller = area.getFromZ() < -50 && area.getToZ() < -50;
+        boolean areZBigger = area.getFromZ() > 50 && area.getToZ() > 50;
+        boolean isZOutside = areZSmaller || areZBigger;
 
-		return !(isXOutside || isYOutside || isZOutside);
-	}
+        return !(isXOutside || isYOutside || isZOutside);
+    }
 
-	public static Set<ThreeDPoint> getCubes(RebootInstruction instruction) {
-		Set<ThreeDPoint> onCubes = new HashSet<>();
-		for (int x = instruction.getFromX(); x <= instruction.getToX(); x++)
-			for (int y = instruction.getFromY(); y <= instruction.getToY(); y++)
-				for (int z = instruction.getFromZ(); z <= instruction.getToZ(); z++)
-					onCubes.add(new ThreeDPoint(x, y, z));
-		return onCubes;
-	}
+    public static Set<ThreeDPoint> getCubes(Cuboid area) {
+        Set<ThreeDPoint> onCubes = new HashSet<>();
+        for (int x = area.getFromX(); x <= area.getToX(); x++)
+            for (int y = area.getFromY(); y <= area.getToY(); y++)
+                for (int z = area.getFromZ(); z <= area.getToZ(); z++)
+                    onCubes.add(new ThreeDPoint(x, y, z));
+        return onCubes;
+    }
 
-	public static List<RebootInstruction> parseInput() {
-		return AdventOfCodeUtils.readInput(Day22Main.class).stream().map(Day22Main::parseInstruction)
-				.collect(Collectors.toList());
-	}
+    public static List<RebootInstruction> parseInput() {
+        return AdventOfCodeUtils.readInput(Day22Main.class).stream().map(Day22Main::parseInstruction)
+                .collect(Collectors.toList());
+    }
 
-	public static RebootInstruction parseInstruction(String line) {
-		Matcher m = LINE_PATTERN.matcher(line);
-		m.find();
+    public static RebootInstruction parseInstruction(String line) {
+        Matcher m = LINE_PATTERN.matcher(line);
+        m.find();
 
-		boolean on = "on".equals(m.group(1));
-		ThreeDPoint from = new ThreeDPoint(Integer.parseInt(m.group(2)), Integer.parseInt(m.group(4)),
-				Integer.parseInt(m.group(6)));
-		ThreeDPoint to = new ThreeDPoint(Integer.parseInt(m.group(3)), Integer.parseInt(m.group(5)),
-				Integer.parseInt(m.group(7)));
+        boolean on = "on".equals(m.group(1));
+        ThreeDPoint from = new ThreeDPoint(Integer.parseInt(m.group(2)), Integer.parseInt(m.group(4)),
+                Integer.parseInt(m.group(6)));
+        ThreeDPoint to = new ThreeDPoint(Integer.parseInt(m.group(3)), Integer.parseInt(m.group(5)),
+                Integer.parseInt(m.group(7)));
 
-		return new RebootInstruction(on, new Cuboid(from, to));
-	}
+        return new RebootInstruction(on, new Cuboid(from, to));
+    }
 
 }
