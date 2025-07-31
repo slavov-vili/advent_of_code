5d15d0caff3015f1983d171d626470287b5e4ae1
vilskin <slavov.vili@gmail.com>
2025-07-31 20:32:19 +0200

    AOC 2021: Add solution for Day 23, Part 2

--- 2021_edition/advent-of-java-2-advent-harder/src/main/java/day23/Day23Main.java
+++ 2021_edition/advent-of-java-2-advent-harder/src/main/java/day23/Day23Main.java
@@ -4,13 +4,13 @@ import utils.AdventOfCodeUtils;
 import utils.PointUtils;
 
 import java.awt.Point;
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Day23Main {
 
@@ -18,22 +18,39 @@ public class Day23Main {
             new Point(1, 3), new Point(1, 5),
             new Point(1, 7), new Point(1, 9)
     );
-    public static final Set<Point> ROOMS = generateRooms(ROOM_FRONTS);
 
     public static void main(String[] args) {
         AmphipodDiagram diagram = parseDiagram();
 
-        System.out.println("Min energy to reorder: " + solveA(diagram));
+        System.out.println("Answer A: " + solveA(diagram));
+
+        System.out.println("Answer B: " + solveB(diagram));
     }
 
     public static int solveA(AmphipodDiagram diagram) {
-        return solveA(diagram, 0, Integer.MAX_VALUE);
+        var newDiagram = diagram;
+        if (diagram.pods.size() > 8) {
+            var newPods = diagram.pods.stream()
+                    .filter(pod -> pod.position.x == 2 || pod.position.x == 5)
+                    .map(pod -> pod.position.x == 5 ? pod.move(new Point(3, pod.position.y)) : pod)
+                    .collect(Collectors.toSet());
+            int newMaxX = diagram.maxX - 2;
+            newDiagram = new AmphipodDiagram(newPods, diagram.freePositions, newMaxX);
+        }
+
+        return solve(newDiagram, 0, Integer.MAX_VALUE, new HashSet<>());
     }
 
-    // TODO: pass curMinEnergy to recursion?
-    public static int solveA(AmphipodDiagram diagram, int curEnergy, int curMinEnergy) {
+    public static int solveB(AmphipodDiagram diagram) {
+        return solve(diagram, 0, Integer.MAX_VALUE, new HashSet<>());
+    }
+
+    public static int solve(AmphipodDiagram diagram, int curEnergy, int curMinEnergy, Set<AmphipodDiagram> seen) {
+        if (seen.contains(diagram))
+            return curMinEnergy;
+
         List<Amphipod> podsToMove = diagram.pods.stream()
-                .filter(pod -> pod.needToMove(diagram.pods)).toList();
+                .filter(pod -> pod.needToMove(diagram)).toList();
         if (podsToMove.isEmpty()) {
             System.out.println("Current final energy: " + curEnergy);
             return curEnergy;
@@ -55,33 +72,20 @@ public class Day23Main {
                 nextPods.remove(pod);
                 nextPods.add(pod.move(nextPos));
 
-                var nextDiagram = new AmphipodDiagram(nextPods, nextFreePositions);
-//                System.out.println();
-//                System.out.println("Current diagram:");
-//                nextDiagram.print();
-//                System.out.println("Current energy: " + nextEnergy);
-                int newMinEnergy = solveA(nextDiagram, nextEnergy, nextMinEnergy);
-                if (newMinEnergy < 0) {
-                    System.out.println();
-                    System.out.println("Next diagram:");
-                    nextDiagram.print();
-                    System.out.println("Negative minEnergy: " + newMinEnergy);
-                }
+                var nextDiagram = new AmphipodDiagram(nextPods, nextFreePositions, diagram.maxX);
+                int newMinEnergy = solve(nextDiagram, nextEnergy, nextMinEnergy, seen);
 
                 if (newMinEnergy < nextMinEnergy) {
                     nextMinEnergy = newMinEnergy;
-//                    System.out.println();
-//                    System.out.println("Current diagram:");
-//                    diagram.print();
-//                    System.out.println("New minEnergy = " + newMinEnergy);
                 }
             }
         }
 
+        seen.add(diagram);
         return nextMinEnergy;
     }
 
-    public record AmphipodDiagram(Set<Amphipod> pods, Set<Point> freePositions) {
+    public record AmphipodDiagram(Set<Amphipod> pods, Set<Point> freePositions, int maxX) {
 
         public boolean isFree(Point pos) {
             return this.freePositions.contains(pos);
@@ -119,12 +123,12 @@ public class Day23Main {
         }
 
         public Set<Point> findNextPositions(AmphipodDiagram diagram) {
-            if (!this.needToMove(diagram.pods))
+            if (!this.needToMove(diagram))
                 return Set.of();
 
             var nextPositions = PointUtils.getPositionsInPaths(this.position, diagram::isFree);
 
-            Predicate<Point> positionValidator = pos -> this.isMyRoom(pos) && this.canEnterRoom(pos, diagram.pods);
+            Predicate<Point> positionValidator = pos -> this.isMyRoom(pos) && this.canEnterRoom(pos, diagram);
             Optional<Point> myRoom = nextPositions.stream().filter(positionValidator).findFirst();
             if (myRoom.isPresent())
                 return Set.of(myRoom.get());
@@ -135,29 +139,29 @@ public class Day23Main {
             return nextPositions.stream().filter(positionValidator).collect(Collectors.toSet());
         }
 
-        public boolean needToMove(Collection<Amphipod> others) {
-            if (isRoomTop(this.position))
-                return !(this.isInMyRoom() && this.isWithRoommate(others));
-            else
-                return !this.isInMyRoom();
+        public boolean needToMove(AmphipodDiagram diagram) {
+            if (this.isInMyRoom())
+                return !this.isAboveRoommates(diagram);
+
+            return true;
         }
 
-        public boolean isWithRoommate(Collection<Amphipod> others) {
-            return wouldBeWithRoommate(this.position, others);
+        public boolean isAboveRoommates(AmphipodDiagram diagram) {
+            return wouldBeAboveRoommates(this.position, diagram);
         }
 
-        // neither can be at the entrance => must be both in the room
-        public boolean wouldBeWithRoommate(Point newPos, Collection<Amphipod> others) {
-            var roommate = others.stream().filter(other -> other.id == this.id && !this.equals(other)).findFirst()
-                    .get();
-            return roommate.position.y == newPos.y && Math.abs(newPos.x - roommate.position.x) == 1;
+        public boolean canEnterRoom(Point pos, AmphipodDiagram diagram) {
+            if (this.isMyRoom(pos))
+                return wouldBeAboveRoommates(pos, diagram);
+
+            return false;
         }
 
-        public boolean canEnterRoom(Point pos, Collection<Amphipod> others) {
-            if (isRoomTop(pos))
-                return wouldBeWithRoommate(pos, others);
-            else
-                return true;
+        public boolean wouldBeAboveRoommates(Point newPos, AmphipodDiagram diagram) {
+            var belowMap = diagram.pods.stream()
+                    .filter(pod -> pod.position.y == newPos.y && pod.position.x > newPos.x)
+                    .collect(Collectors.toMap(pod -> pod.position.x, pod -> pod.id));
+            return IntStream.rangeClosed(newPos.x + 1, diagram.maxX).allMatch(pos -> belowMap.containsKey(pos) && belowMap.get(pos) == this.id);
         }
 
         public boolean isInMyRoom() {
@@ -165,7 +169,7 @@ public class Day23Main {
         }
 
         public boolean isMyRoom(Point pos) {
-            return ROOMS.contains(pos) && pos.y == 2 * (this.id - 0x41 + 1) + 1;
+            return isRoom(pos) && pos.y == 2 * (this.id - 0x41 + 1) + 1;
         }
 
         public int calcEnergyTo(Point destination) {
@@ -180,7 +184,7 @@ public class Day23Main {
     }
 
     public static int calcDistance(Point a, Point b) {
-        return (int) Math.abs(a.y - b.y) + calcDistToHallway(a) + calcDistToHallway(b);
+        return Math.abs(a.y - b.y) + calcDistToHallway(a) + calcDistToHallway(b);
     }
 
     public static int calcDistToHallway(Point p) {
@@ -188,38 +192,32 @@ public class Day23Main {
     }
 
     public static boolean isRoom(Point pos) {
-        return ROOMS.contains(pos);
+        return pos.x > 1;
     }
 
     public static boolean isHallway(Point pos) {
         return !isRoom(pos);
     }
 
-    public static boolean isRoomTop(Point pos) {
-        return isRoom(pos) && ROOM_FRONTS.contains(new Point(pos.x - 1, pos.y));
-    }
-
-    public static Set<Point> generateRooms(Set<Point> roomFronts) {
-        return roomFronts.stream()
-                .flatMap(front -> Set.of(new Point(front.x + 1, front.y), new Point(front.x + 2, front.y)).stream())
-                .collect(Collectors.toSet());
-    }
-
     public static AmphipodDiagram parseDiagram() {
         List<String> input = AdventOfCodeUtils.readInput(Day23Main.class);
         Set<Amphipod> pods = new HashSet<>();
         Set<Point> freePositions = new HashSet<>();
+        int maxX = 0;
         for (int row = 0; row < input.size(); row++) {
             for (int col = 0; col < input.get(row).length(); col++) {
                 char curChar = input.get(row).charAt(col);
                 Point curPos = new Point(row, col);
                 if (curChar == '.')
                     freePositions.add(curPos);
-                else if (Character.isLetter(curChar))
+                else if (Character.isLetter(curChar)) {
                     pods.add(new Amphipod(curChar, curPos));
+                    if (curPos.x > maxX)
+                        maxX = curPos.x;
+                }
             }
         }
 
-        return new AmphipodDiagram(pods, freePositions);
+        return new AmphipodDiagram(pods, freePositions, maxX);
     }
 }
