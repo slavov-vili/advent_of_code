7d3a6079055a13239dce153da7abb0add82d7049
vilskin <slavov.vili@gmail.com>
2025-12-02 22:52:59 +0100

    AOC25, Day02: Add solution to part 2

--- 2025_edition/day02/Main.kts
+++ 2025_edition/day02/Main.kts
@@ -4,16 +4,35 @@ import java.io.File
 
 fun String.toRange(): LongRange {
     val (start, end) = this.split("-").map(String::toLong)
-    return LongRange(start, end)
+    return start..end
 }
 
 fun Long.isInvalidID(size: Int): Boolean {
     val str = this.toString()
-    val mid = str.length / size
-    return str.substring(0, mid) == str.substring(mid, str.length)
+    if (str.length % size != 0) {
+        return false
+    }
+
+    val substringRanges = (0..(str.length / size - 1)).map { i ->
+        (size * i)..(size * (i + 1) - 1)
+    }
+
+    return substringRanges.map(str::substring).zipWithNext().all { pair -> pair.first == pair.second }
+}
+
+fun Long.isInvalidForA(): Boolean {
+    return this.isInvalidID(2)
+}
+
+fun Long.isInvalidForB(): Boolean {
+    val str = this.toString()
+    return (1..str.length / 2).any { this.isInvalidID(it) }
 }
 
 val ranges = File("input.txt").readLines().first().split(",").map(String::toRange)
 
-ranges.sumOf { it.filter { it.isInvalidID(2) }.sum() }
+val sumA = ranges.sumOf { it.filter(Long::isInvalidForA).sum() }
+println("Sum A: $sumA")
 
+val sumB = ranges.sumOf { it.filter(Long::isInvalidForB).sum() }
+println("Sum B: $sumB")
