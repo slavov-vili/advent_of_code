06138e99c32d143463b1f27844f0e4ebbf4cb58f
vilskin <slavov.vili@gmail.com>
2025-12-02 22:53:15 +0100

    AOC25: Add script for generating initial files

---
+++ 2025_edition/start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DAY="day$1"
+MAIN="$DAY/Main.kts"
+INPUT="$DAY/input.txt"
+OUTPUT="$DAY/output.txt"
+
+mkdir "$DAY"
+touch "$MAIN" "$INPUT" "$OUTPUT"
+
+
+
+cat << EOF > "$MAIN"
+package $DAY
+
+import java.io.File
+
+val input = File("input.txt").readLines()
+EOF
+
+
+
+cat << EOF > "$OUTPUT"
+A: 
+
+B: 
+EOF
