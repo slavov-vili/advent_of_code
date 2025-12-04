a1ed2960329ed8057653be58b95a2e2fbe49933f
vilskin <slavov.vili@gmail.com>
2025-12-04 21:52:17 +0100

    AOC25, Day04: Add solution to part 1

---
+++ 2025_edition/day04/Main.kts
@@ -0,0 +1,58 @@
+package day04
+
+import java.io.File
+
+typealias Direction = Pair<Int, Int>
+typealias Position = Pair<Int, Int>
+typealias Limits = Pair<IntRange, IntRange>
+typealias PaperRollMap = List<String>
+
+val directions: List<Direction> = listOf(Direction(-1, 0), Direction(-1, 1), Direction(0, 1), Direction(1, 1),
+    Direction(1, 0), Direction(1, -1), Direction(0, -1), Direction(-1, -1))
+
+fun Position.move(other: Direction): Position {
+    return Position(this.first + other.first, this.second + other.second)
+}
+
+fun Position.isValid(limits: Limits): Boolean {
+    return this.first in limits.first && this.second in limits.second
+}
+
+fun Position.getValidNeighbors(paperRollMap: PaperRollMap, all: Boolean): List<Position> {
+    val limits = paperRollMap.getLimits()
+    val step = if (all) 1 else 2
+    return IntRange(0, directions.count() - 1).step(step)
+        .map { this.move(directions[it]) }
+        .filter { it.isValid(limits) }
+}
+
+fun Position.isAccessible(paperRollMap: PaperRollMap): Boolean {
+    return this.getValidNeighbors(paperRollMap, true)
+        .filter { paperRollMap.isRoll(it) }
+        .count() < 4
+}
+
+fun PaperRollMap.isRoll(pos: Position): Boolean {
+    return this[pos.first][pos.second] == '@'
+}
+
+fun PaperRollMap.getLimits(): Limits {
+    return Limits(IntRange(0, this.count() - 1), IntRange(0, this[0].length - 1))
+}
+
+fun PaperRollMap.getPositions(): List<Position> {
+    val limits = this.getLimits()
+    return IntRange(0, limits.second.endInclusive)
+        .flatMap { col ->
+            IntRange(0, limits.first.endInclusive)
+            .map { line -> Position(line, col) } }
+}
+
+fun PaperRollMap.findAccessibleRollPositions(): List<Position> {
+    return this.getPositions().filter { input.isRoll(it) && it.isAccessible(input) }
+}
+
+val input: PaperRollMap = File("input.txt").readLines().filterNot(String::isEmpty)
+
+val accessible = input.findAccessibleRollPositions()
+println("Accessible rolls: ${accessible.count()}")
