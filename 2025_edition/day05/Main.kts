777e65c71d817fca67cfcd2ec3bb9dc92f7be595
vilskin <slavov.vili@gmail.com>
2025-12-05 22:59:19 +0100

    AOC25, Day05: Add solution to part 2

--- 2025_edition/day05/Main.kts
+++ 2025_edition/day05/Main.kts
@@ -7,10 +7,39 @@ fun String.toRange(): LongRange {
     return LongRange(start.toLong(), end.toLong())
 }
 
+val rangeComparator = compareBy<LongRange> { it.start }.thenBy { it.endInclusive }
+
+fun LongRange.mergeWith(other: LongRange): List<LongRange> {
+    val overlap = other.start in this || this.start in other
+    if (overlap) {
+        return listOf(LongRange(minOf(this.start, other.start), maxOf(this.endInclusive, other.endInclusive)))
+    }
+
+    val lessThanOrEqual = rangeComparator.compare(this, other) <= 0
+    val min = if (lessThanOrEqual) this else other
+    val max = if (lessThanOrEqual) other else this
+    return listOf(min, max)
+}
+
+fun List<LongRange>.sortAndCompress(): List<LongRange> {
+    return this.sortedWith(rangeComparator)
+        .fold(listOf<LongRange>()) { ranges, next ->
+            val prev = if (ranges.isEmpty()) next else ranges.last()
+            ranges.subList(0, maxOf(0, ranges.count()-1)) + prev.mergeWith(next)
+        }
+}
+
+fun List<LongRange>.countAll(): Long {
+    return this.map { it.endInclusive - it.start + 1}.sum()
+}
+
 val input = File("input.txt").readLines().filterNot(String::isEmpty)
 
 val freshRanges = input.filter { line -> line.contains("-")}.map(String::toRange)
 val available = input.filterNot { line -> line.contains("-") }.map(String::toLong)
 
 val fresh = available.filter { x -> freshRanges.any { range -> x in range } }
-println("Fresh ingredient count: ${fresh.count()}")
+println("Available fresh ingredient count: ${fresh.count()}")
+
+val allFresh = freshRanges.sortAndCompress()
+println("All fresh ingredient count: ${allFresh.countAll()}")
