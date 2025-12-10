f99c097d6084b50554793837a57dcc9fbb4fc7b7
vilskin <slavov.vili@gmail.com>
2025-12-10 22:44:49 +0100

    AOC25, Day08: Add solution to part 2 - Replace .slice with .subList

--- 2025_edition/day08/Main.kts
+++ 2025_edition/day08/Main.kts
@@ -16,16 +16,15 @@ data class Position3D(val x: Long, val y: Long, val z: Long) {
 
 data class ClosestTuple(val box1: Position3D, val box2: Position3D, val distance: Double)
 
-data class CircuitMap(val wrapped: Map<Position3D, Long?>) {
-    val values = this.wrapped.values
+data class CircuitMap(val wrapped: Map<Position3D, Long?>, val lastMerged: Pair<Position3D, Position3D>?) {
+    val values: Collection<Long?> = this.wrapped.values
 
     companion object {
         fun of(vararg entries: Pair<Position3D, Long?>): CircuitMap {
-            return CircuitMap(mapOf(*entries))
+            return CircuitMap(mapOf(*entries), null)
         }
     }
 
-    // TODO: getOrDefault, containsKey, plus, merge
     fun getOrDefault(key: Position3D, defaultValue: Long?): Long? {
         return this.wrapped.getOrDefault(key, defaultValue)
     }
@@ -34,13 +33,19 @@ data class CircuitMap(val wrapped: Map<Position3D, Long?>) {
         return this.wrapped.containsKey(key)
     }
 
-    operator fun plus(other: CircuitMap): CircuitMap {
-        return CircuitMap(this.wrapped + other.wrapped)
+    operator fun plus(boxPair: Pair<Pair<Position3D, Long?>, Pair<Position3D, Long?>>): CircuitMap {
+        return CircuitMap(this.wrapped + listOf(boxPair.first, boxPair.second), Pair(boxPair.first.first, boxPair.second.first))
     }
 
-    fun merge(circuits: Set<Long>): CircuitMap {
-        val mergedCircuit = circuits.minOf { it }
-        return CircuitMap(this.wrapped.mapValues { if (circuits.contains(it.value)) mergedCircuit else it.value })
+    fun merge(boxPair: Pair<Pair<Position3D, Long>, Pair<Position3D, Long>>): CircuitMap {
+        val (box1, box2) = boxPair
+        val mergedCircuit = minOf(box1.second, box2.second)
+        return CircuitMap(this.wrapped.mapValues { if (it.value == box1.second || it.value == box2.second) mergedCircuit else it.value },
+            Pair(box1.first, box2.first))
+    }
+
+    fun isFullyMerged(totalBoxCount: Long): Boolean {
+        return this.wrapped.count().toLong() == totalBoxCount && this.wrapped.values.distinct().count() == 1
     }
 }
 
@@ -60,25 +65,22 @@ fun <T> findCircuits(closestTuples: List<ClosestTuple>, condition: (CircuitFindi
 
 tailrec fun <T> findCircuits(data: CircuitFindingData, condition: (CircuitFindingData) -> Boolean,
     resultExtractor: (CircuitFindingData) -> T): T {
-    val (curClosestTuples, curCircuitMap, i) = data
     if (condition(data)) {
         return resultExtractor(data)
     }
 
+    val (curClosestTuples, curCircuitMap, i) = data
     val (minBox1, minBox2) = curClosestTuples[0]
 
     val circuit1 = curCircuitMap.getOrDefault(minBox1, null)
     val circuit2 = curCircuitMap.getOrDefault(minBox2, i)
-    // println("\nMin pair: $minBox1 -> $minBox2")
-    // println("Circuits: $circuit1 -> $circuit2")
 
-    val nextClosestTuples = curClosestTuples.slice(IntRange(1, curClosestTuples.count() - 1))
+    val nextClosestTuples = curClosestTuples.subList(1, curClosestTuples.count() - 1)
     val nextCircuitMap =  if (curCircuitMap.containsKey(minBox1) && curCircuitMap.containsKey(minBox2)) {
-        curCircuitMap.merge(setOf(circuit1!!, circuit2!!))
+        curCircuitMap.merge(Pair(Pair(minBox1, circuit1!!), Pair(minBox2, circuit2!!)))
     } else {
         val circuitId = circuit1 ?: circuit2
-        // println("Circuit ID is $circuitId")
-        curCircuitMap + CircuitMap.of(minBox1 to circuitId, minBox2 to circuitId)
+        curCircuitMap + Pair(minBox1 to circuitId, minBox2 to circuitId)
     }
 
     val nextData = CircuitFindingData(nextClosestTuples, nextCircuitMap, i + 1)
@@ -95,11 +97,14 @@ val closestTuples = IntRange(0, input.count() - 2).flatMap { i ->
     }
     .filterNot { it.distance == 0.0 }
     .sortedBy { it.distance }
-// closestTuples.forEach(::println)
 
 val circuitCountMap = findCircuits(closestTuples.take(1000), { it.closestTuples.isEmpty() }, { it.circuitMap })
     .values.groupBy { it }
     .mapValues { (_, valueList) -> valueList.count().toLong() }
 
 val sizesMultiplied = circuitCountMap.values.sortedDescending().take(3).reduce { a, b -> a * b }
-println(sizesMultiplied)
+println("Solution A: $sizesMultiplied")
+
+val fullMergePair = findCircuits(closestTuples, { it.circuitMap.isFullyMerged(input.count().toLong()) }, { it.circuitMap.lastMerged })
+val xMultiplied = (fullMergePair?.first?.x ?: 0) * (fullMergePair?.second?.x ?: 0)
+println("Solution B: $xMultiplied")
